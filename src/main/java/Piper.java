38e2448fba8858c99914e0cb9fce6ef720e47515
Ong Yi Xin <yixinong34@gmail.com>
2025-08-15 18:48:19 +0800

    Implement Level-1 Echo

--- src/main/java/Piper.java
+++ src/main/java/Piper.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Piper {
     public static void main(String[] args) {
-        String chatbot_Name = "Piper";
+        final String chatbot_Name = "Piper";
+        boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+
+        // greet user
         System.out.println(
                 "____________________________________________________________\n"
                 + "Hi! " + chatbot_Name + " here.\n"
                 + "What shall we do today?\n"
                 + "____________________________________________________________\n"
-                + "Til next time!"
         );
+
+        while (!exit) { // user is active
+            String input = scanner.nextLine();
+            if (input.equals("bye")) { // user is inactive
+                System.out.println(
+                        "Til next time!"
+                );
+                exit = true;
+            } else {
+                System.out.println(input); // echo user
+            }
+        }
     }
 }
