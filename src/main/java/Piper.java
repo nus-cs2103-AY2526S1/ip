caa4cb06bfff6551d9617d22548fae4a3fbbdb8a
Ong Yi Xin <yixinong34@gmail.com>
2025-08-15 18:04:48 +0800

    Implement Level-0 Rename, Greet, Exit

---
+++ src/main/java/Piper.java
@@ -0,0 +1,12 @@
+public class Piper {
+    public static void main(String[] args) {
+        String chatbot_Name = "Piper";
+        System.out.println(
+                "____________________________________________________________\n"
+                + "Hi! " + chatbot_Name + " here.\n"
+                + "What shall we do today?\n"
+                + "____________________________________________________________\n"
+                + "Til next time!"
+        );
+    }
+}
