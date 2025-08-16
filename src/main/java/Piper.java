a43f42fd8f589755d007b4d2aa5942a314b0f746
Ong Yi Xin <yixinong34@gmail.com>
2025-08-16 17:38:56 +0800

    Add ASCII art for greet and exit

--- src/main/java/Piper.java
+++ src/main/java/Piper.java
@@ -6,19 +6,41 @@ public class Piper {
         boolean exit = false;
         Scanner scanner = new Scanner(System.in);
 
+        // ascii art
+        final String ascii_Greet =
+                " _______                                   \n" +
+                "|_   __ \\ ( ) .\\                         \n" +
+                "  | |__) |__  | ''\\   .---.  _ .--.       \n" +
+                "  |  ___/[  | | /'\\\\ / /__\\\\[  /''\\|  \n" +
+                " _| |_    | | | \\_/ || \\__.'| |          \n" +
+                "|_____|  [___]| |\\_/  \\___/[___]         \n" +
+                "           / /| | \\                       \n" +
+                "           [ \\] |  ]                      \n" +
+                "            \\__ __/      (l_              \n" +
+                "              | |        <'  }             \n" +
+                "            [___/        (  (_\\.&         \n" +
+                "--------------------<>----''--\\\\---------\n" +
+                "                                \\         \n";
+        final String ascii_Exit =
+                "             ______         ___,           \n" +
+                "              `--- \\   _))/.--`           \n" +
+                "            ,__`--. \\/  '>--`             \n" +
+                "----------- `._.-.      /``----------------\n" +
+                "                  '.__.'                   \n" +
+                "                   ' '                     \n";
+
         // greet user
         System.out.println(
-                "____________________________________________________________\n"
-                + "Hi! " + chatbot_Name + " here.\n"
-                + "What shall we do today?\n"
-                + "____________________________________________________________\n"
+                ascii_Greet +
+                        "Hi! " + chatbot_Name + " here.\n" +
+                        "What shall we do today?\n"
         );
 
         while (!exit) { // user is active
             String input = scanner.nextLine();
             if (input.equals("bye")) { // user is inactive
                 System.out.println(
-                        "Til next time!"
+                        "Til next time!\n" + ascii_Exit
                 );
                 exit = true;
             } else {
