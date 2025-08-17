2ab3adda29b984300726b461ccf51afd65c09111
Zisaac99 <zisaac2468@gmail.com>
2025-08-17 23:37:33 +0800

    TaskList.java: Add method to print all tasks

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -11,4 +11,10 @@ public class TaskList {
     public void addTask (String name) {
         this.tasks.add(new Task(name));
     }
+
+    public void listAllTasks () {
+        for (int i = 0; i < this.tasks.size(); i++) {
+            System.out.printf("%d. %s", i + 1, this.tasks.get(i));
+        }
+    }
 }
