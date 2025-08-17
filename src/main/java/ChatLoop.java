0e17bd7c792a6353fe926cd451cd8fb47c85207e
Zisaac99 <zisaac2468@gmail.com>
2025-08-17 22:46:43 +0800

    ChatLoop.java: Add function that implements the echo feature

--- src/main/java/ChatLoop.java
+++ src/main/java/ChatLoop.java
@@ -5,6 +5,26 @@ public class ChatLoop {
 
     }
 
+    public void run() {
+        boolean isFinished = false;
+        Scanner scanner = new Scanner(System.in);
+
+        printFormattedMessage("Hello! My name is Zell\n How can I help you?");
+        while (!isFinished) {
+            String userInput = scanner.nextLine();
+
+            switch (userInput) {
+            case "bye":
+                printFormattedMessage("Goodbye. Hope to see you again soon!");
+                isFinished = true;
+                break;
+            default:
+                printFormattedMessage(userInput);
+                break;
+            }
+        }
+    }
+
     public void printFormattedMessage(String message) {
         String formattedMessage =
                 "____________________________________________________________\n " +
