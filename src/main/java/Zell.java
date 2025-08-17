abde5056f87a5c5daef8dc3e3927ddfcb1e0c6d6
Zisaac99 <zisaac2468@gmail.com>
2025-08-17 22:48:10 +0800

    Zell.java: Change main() to use ChatLoop class run()

--- src/main/java/Zell.java
+++ src/main/java/Zell.java
@@ -1,10 +1,6 @@
 public class Zell {
     public static void main(String[] args) {
-        String greetingMessage =
-                "____________________________________________________________\n" +
-                " Hello! My name is Zell\n" +
-                " How can I help you?\n" +
-                "____________________________________________________________\n\n";
-        System.out.println(greetingMessage);
+        ChatLoop chatLoop = new ChatLoop();
+        chatLoop.run();
     }
 }
