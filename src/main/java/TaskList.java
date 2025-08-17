27835ce51af6045a4ca090af06ccd657d6ff7f9b
Zisaac99 <zisaac2468@gmail.com>
2025-08-17 23:33:36 +0800

    TaskList.java: Add TaskList class which stores user's tasks

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class TaskList {
+    private List<Task> tasks;
+
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+}
