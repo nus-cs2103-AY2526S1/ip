0d87337b0a60fed1aaf0aca9e2153fe3ea21495d
Zisaac99 <zisaac2468@gmail.com>
2025-08-18 02:29:41 +0800

    Task.java: Add instance variable to indicate if task is done

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,15 @@
 public class Task {
     private String name;
+    private boolean isDone;
 
     public Task (String name) {
         this.name = name;
+        this.isDone = false;
     }
 
+    public void toggleIsDone() {
+        this.isDone = !this.isDone;
+    }
     @Override
     public String toString() {
         return this.name;
