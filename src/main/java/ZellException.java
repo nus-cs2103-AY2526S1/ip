be4e4d3448ed939d89fdbf22f06d2d09cb92fd4b
Zisaac99 <zisaac2468@gmail.com>
2025-08-19 01:35:55 +0800

    ZellException.java: Create custom exception class for Zell

---
+++ src/main/java/ZellException.java
@@ -0,0 +1,10 @@
+public class ZellException extends Exception{
+    public ZellException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return "An error occurred: " + getMessage();
+    }
+}
