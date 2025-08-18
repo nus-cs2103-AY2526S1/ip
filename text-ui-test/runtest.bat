eed617be11c630fe901f455a23a5b944015f8ab3
Zisaac99 <zisaac2468@gmail.com>
2025-08-18 23:52:23 +0800

    runtest.bat: Modify bat file to accept argument of filename

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -3,8 +3,11 @@
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
 
+REM Solution below to pass in argument to .bat file adapted from: https://stackoverflow.com/questions/26551/how-can-i-pass-arguments-to-a-batch-file
+set fileName=%1
+
 REM delete output from previous run
-if exist ACTUAL.TXT del ACTUAL.TXT
+if exist ACTUAL_%fileName%.TXT del ACTUAL_%fileName%.TXT
 
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
@@ -15,7 +18,7 @@ IF ERRORLEVEL 1 (
 REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Duke < input.txt > ACTUAL.TXT
+java -classpath ..\bin Zell < input_%fileName%.txt > ACTUAL_%fileName%.TXT
 
 REM compare the output to the expected output
-FC ACTUAL.TXT EXPECTED.TXT
+FC ACTUAL_%fileName%.TXT EXPECTED_%fileName%.TXT
