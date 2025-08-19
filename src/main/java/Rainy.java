8ff738aff71ebc08cb378b51e654f12f27b31cba
reineoeq <e1354688@u.nus.edu>
2025-08-19 14:02:16 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Rainy.java
@@ -0,0 +1,12 @@
+public class Rainy {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        System.out.println(line
+                + "Hello! I'm Rainy\n"
+                + "What can I do for you?\n"
+                + line
+                + "Bye. Hope to see you again soon!"
+                + "\n"
+                + line);
+    }
+}
