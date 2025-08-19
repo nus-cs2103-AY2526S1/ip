ae6f7dcdcde3d50bde843c85b41c845de699fcd0
reineoeq <e1354688@u.nus.edu>
2025-08-19 14:11:45 +0800

    Level 1: Echo

--- src/main/java/Rainy.java
+++ src/main/java/Rainy.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
 public class Rainy {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String line = "____________________________________________________________\n";
         System.out.println(line
                 + "Hello! I'm Rainy\n"
                 + "What can I do for you?\n"
-                + line
-                + "Bye. Hope to see you again soon!"
-                + "\n"
                 + line);
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(line + " Bye. Hope to see you again soon!\n" + line);
+                break;
+            }
+            System.out.println(line + input + "\n" + line);
+        }
+        sc.close();
     }
 }
