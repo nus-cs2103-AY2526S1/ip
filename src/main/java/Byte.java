a8e1abe0e70f1e2242896032fc48c2250a0c7b81
ArtistZ <limhur.ng@gmail.com>
2025-08-19 15:33:55 +0800

    Create initial skeleton version of Byte

---
+++ src/main/java/Byte.java
@@ -0,0 +1,8 @@
+public class Byte {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        String greetMessage = "Hello! I'm Byte.\nWhat can I do for you?\n";
+
+        System.out.println(line + greetMessage + line);
+    }
+}
