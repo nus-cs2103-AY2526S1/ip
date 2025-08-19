18c6d19f8004cb04fee0d00e3032bf4683b33226
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2025-08-19 20:16:45 +0800

    Update bot to have Task functionality
    - Update Messages
    - Create Task class
    - Update main code to use Task Class
    - Create Main.java to run the bot

--- src/main/java/BeeBong.java
+++ src/main/java/BeeBong.java
@@ -1,69 +1,108 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class BeeBong {
-    private static final String newLine = "____________________________________________________________";
-    private static final String errorMsg = "Bong! Something went boom in B. Bong’s circuits.";
-    private static int currCommand = 0;
+    private final String newLine = "____________________________________________________________";
+//    private final String genericErrorMsg = "Bong! Something went boom in B. Bong’s circuits. Try again!";
+    private int currTask = 0;
+    private final Task[] tasklist = new Task[100];
 
-    public static void BotMessage(String message) {
-        System.out.println(newLine);
+    private void botMessage(String message) {
+        System.out.println(this.newLine);
         System.out.println(message);
-        System.out.println(newLine);
+        System.out.println(this.newLine);
     }
 
-    public static void BotMessageList(String[] messages) {
-        System.out.println(newLine);
-        for (int i = 0; i < currCommand; i++) {
-            System.out.println((i + 1) + ". " + messages[i]);
+    private void botErrorMessage(String errorMessage) {
+        botMessage("Bong Alert! "+errorMessage);
+    }
+
+    private void greetingMessage() {
+        botMessage("Ding Dong! Guess who? It’s B. Bong!\nHow can I bong your day brighter?");
+    }
+
+    private void exitMessage() {
+        botMessage("Ding ding! Time to go. See you soon!");
+    }
+
+    private void listTasks() {
+        // If there are no Tasks to list
+        if (this.currTask == 0) {
+            botMessage("Bong! I searched high and low… still nothing to show right now.");
+            return;
         }
-        System.out.println(newLine);
+        System.out.println(this.newLine);
+        System.out.println("Bing! Here’s what’s buzzing in your list, courtesy of B. Bong:");
+        for (int i = 0; i < this.currTask; i++) {
+            System.out.println((i + 1) + ". " + this.tasklist[i]);
+        }
+        System.out.println(this.newLine);
     }
 
-    public static void GreetingMessage() {
-        BotMessage("Ding Dong! Guess who? It’s B. Bong!\nHow can I bong your day brighter?");
+    private void markTaskAs(String params, boolean status) {
+        // Check if params were provided
+        if (params == null) {
+            botErrorMessage("You forgot the task number!");
+            return;
+        }
+        try {
+            int taskNum = Integer.parseInt(params) - 1;
+            //Check for valid task number
+            if (taskNum < 0 || taskNum >= this.currTask) {
+                botErrorMessage("That task number doesn’t exist. Try a real one!");
+                return;
+            }
+            //Mark Task as Completed/Incomplete
+            if (status) this.tasklist[taskNum].markCompleted();
+            else this.tasklist[taskNum].markIncomplete();
+            botMessage("Bing! Task #" + (taskNum + 1) + " marked as " + (status ? "complete" : "incomplete") + "!");
+        } catch (NumberFormatException e) {
+            botErrorMessage("That task number doesn’t exist. Try a real one!");
+        }
     }
 
-    public static void ExitMessage() {
-        BotMessage("Bye. Hope to see you again soon!");
-    }
-
-    public static void main(String[] args) {
-        GreetingMessage();
-        String commands = "List - lists out previous inputs\nBye / Q - exit ";
-        String[] userInputs = new String[100];
+    public void Start() {
+        greetingMessage();
 
         boolean running = true;
         while (running) {
             // Ask for user input
-            System.out.println(commands);
+            String commandList = """
+                    List - lists out previous inputs
+                    Mark [task no.] - mark the task with the given number as completed
+                    Unmark [task no.] - mark the task with the given number as incomplete
+                    Bye / Q - exit
+                    Enter a new Task name or Command""";
+            botMessage(commandList);
             System.out.print(">>> ");
             Scanner s = new Scanner(System.in);
             String input = s.nextLine();
 
             // Check for Commands
-            switch (input.toLowerCase()) {
-                // Bye / Q - Exit
+            String[] commandParts = input.toLowerCase().split(" ", 2);
+            String command = commandParts[0];
+            String params = commandParts.length > 1 ? commandParts[1] : null;
+            switch (command) {
                 case "bye":
                 case "q":
                     running = false;
                     break;
-                // List - List all previous user inputs
                 case "list":
-                    if (currCommand == 0) {
-                        BotMessage("Oops! I searched high and low… still nothing to show right now.");
-                        break;
-                    }
-                    BotMessageList(userInputs);
+                    // List all Tasks
+                    listTasks();
+                    break;
+                case "mark":
+                case "unmark":
+                    // Mark the task as complete or incomplete
+                    markTaskAs(params, command.equals("mark"));
                     break;
                 default:
-                    userInputs[currCommand] = input;
-                    BotMessage("Added: "+input);
-                    currCommand += 1;
+                    // Add the input as a new Task to Task List
+                    this.tasklist[currTask] = new Task(input);
+                    botMessage("Added New Task: "+input);
+                    this.currTask += 1;
             }
         }
 
-        ExitMessage();
+        exitMessage();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    private String name;
+    private boolean completed;
+
+    public Task(String name) {
+        this.name = name;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return this.completed;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+
+    public void markIncomplete() {
+        this.completed = false;
+    }
+
+    @Override
+    public String toString() {
+        return "["+(this.completed ? "X" : " " )+"] "+this.name;
+    }
+}
