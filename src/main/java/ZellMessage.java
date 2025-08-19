d6dfb096995501cf9de84a8f9cf457da07c6b212
Zisaac99 <zisaac2468@gmail.com>
2025-08-19 21:23:12 +0800

    ZellMessage.java: Add enum for default/fixed messages in Zell

--- src/main/java/ZellMessage.java
+++ src/main/java/ZellMessage.java
@@ -1,3 +1,20 @@
 public enum ZellMessage {
+    WELCOME_MESSAGE("Hello! My name is Zell\n How can I help you?"),
+    UNKNOWN_COMMAND(" is an unknown command. Please try again with a valid command"),
+    TASK_ADDED("Noted. The following task has been added:\n "),
+    TASK_REMOVED("Noted. The following task has been removed:\n "),
+    GOODBYE("Goodbye. Hope to see you again soon!"),
+    LIST("Currently you have added this tasks to your list:\n"),
+    TASK_MARKED("Nice! I've marked this task as done:\n "),
+    TASK_UNMARKED("OK, I've marked this task as not done yet:\n ");
 
+    private String message;
+
+    ZellMessage(String message) {
+        this.message = message;
+    }
+
+    public String message() {
+        return this.message;
+    }
 }
