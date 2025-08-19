d691a806f2c15d0341637a5f60c6b44b05d6ed61
Kineeeeee <e1375463@u.nus.edu>
2025-08-19 23:06:32 +0800

    Level-0

---
+++ src/main/java/Sora.java
@@ -0,0 +1,15 @@
+public class Sora {
+    public static void main(String[] args) {
+        String logo = "  ____                 \n"
+                + " / ___|  ____ ______ __ \n"
+                + " \\___ \\ / __ \\| '__/ _  \\\n"
+                + "  ___\\ | |__| | | | |_| |\n"
+                + " |____/ \\____/|_|  \\___/|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Sora");
+        System.out.println("What can I do for you?");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
