1b6884361cd425beb0166d483b83680a31f3a3c7
reineoeq <e1354688@u.nus.edu>
2025-08-19 14:31:54 +0800

    Level 3: Mark as Done

--- src/main/java/Rainy.java
+++ src/main/java/Rainy.java
@@ -4,7 +4,7 @@ public class Rainy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = "____________________________________________________________\n";
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         /** intro**/
@@ -20,13 +20,21 @@ public class Rainy {
                 System.out.println(line + " Bye. Hope to see you again soon!\n" + line);
                 break;
             } else if (input.equals("list")) {
-                System.out.println(line);
+                System.out.println(line + "Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 System.out.println(line);
+            } else if (input.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskNumber].markAsDone();
+                System.out.println(line + "Nice! I've marked this task as done:" + tasks[taskNumber]);
+            } else if (input.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[taskNumber].unmark();
+                System.out.println(line + "OK, I've marked this task as not done yet:" + tasks[taskNumber]);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println(line + " added: " + input +"\n" + line);
             }
