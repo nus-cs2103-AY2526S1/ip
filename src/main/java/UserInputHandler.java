229e15b1e2007819503daa6e8ae94ea05c034f13
Zisaac99 <zisaac2468@gmail.com>
2025-08-19 12:38:38 +0800

    UserInputHandler.java: Shift over input handling logic from ChatLoop.java
    Currently handling the user inputs is done in
    ChatLoop:handleUserInput(). However, with the addition of new commands
    and handling of exceptions that method is getting to complex.
    
    Let's,
    * Shift all the logic to handle user inputs to UserInputHandler.java
    * Create a method handleInput for handling user inputs
    * Create a method handleCommand to determine what to do with commands
    * Create a method handleTaskCommands to handle commands under Task like
      todo, deadline, event
    * Create a method createTask to handle creation of task
    * Create a method getEndProgram so that ChatLoop knows if based on
      the current user input if the ChatLoop should terminate
    * Create a method handleBye to handle the bye command
    * Create a method handleList to handle the list command
    * Create a method handleMarkOrUnMark to handle mark/unmark command
    * Create all the respective methods to check and throw exceptions for
      each command.
    
    Abstracting the logic to handle user's commands to a class a good practice
    here. Since it will improve maintainability for handling user inputs in
    the future since it will continue to get even more complex when more
    requirements are added. Splitting the methods as stated above is good so
    that we can adhere to SLAP.

---
+++ src/main/java/UserInputHandler.java
@@ -0,0 +1,257 @@
+public class UserInputHandler {
+    private boolean endProgram;
+
+    public UserInputHandler() {
+        this.endProgram = false;
+    }
+
+    public String handleInput(String userInput, TaskList taskList) {
+        int firstSpaceIndex = userInput.indexOf(" ");
+
+        String command = firstSpaceIndex != -1 ? userInput.substring(0, firstSpaceIndex) : userInput;
+        String output;
+
+        // Handle exceptions;
+        try {
+            output = handleCommand(userInput, command, taskList);
+        } catch (ZellException ze) {
+            output = ze.toString();
+        }
+
+        return output;
+    }
+
+    public String handleCommand(String userInput, String command, TaskList taskList) throws ZellException {
+        int firstSpaceIndex = userInput.indexOf(" ");
+        String output;
+
+        switch (command) {
+        case "bye":
+            output = handleBye(firstSpaceIndex, command);
+            break;
+        case "list":
+            output = handleList(firstSpaceIndex, command, taskList);
+            break;
+        case "mark":
+            // Fallthrough
+        case "unmark":
+            output = handleMarkOrUnMark(command, userInput, firstSpaceIndex, taskList);
+            break;
+        case "todo":
+            // Fallthrough
+        case "deadline":
+            // Fallthrough
+        case "event":
+            output = handleTaskCommands(userInput, command, firstSpaceIndex, taskList);
+            break;
+        default:
+            throw new ZellException(command + " is an unknown command. Please try again with a valid command");
+        }
+
+        return output;
+    }
+
+    public String handleTaskCommands(String userInput, String command, int firstSpaceIndex, TaskList taskList)
+            throws ZellException {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Noted. The following task has been added:\n ");
+
+        Task task = createTask(userInput, command, firstSpaceIndex);
+
+        taskList.addTask(task);
+
+        stringBuilder.append(task);
+
+        String numberOfTaskMessage = String.format("\nThere are currently %d task in the list.", taskList.numberOfTask());
+
+        stringBuilder.append(numberOfTaskMessage);
+
+        return stringBuilder.toString();
+    }
+
+    public Task createTask(String userInput, String command, int firstSpaceIndex) throws ZellException {
+        handleNoSpacesForTask(command, firstSpaceIndex);
+
+        String userInputSecondHalf = userInput.substring(firstSpaceIndex + 1);
+        Task task;
+
+        if (command.equals("todo")) {
+            task = new ToDo(userInputSecondHalf);
+        } else if (command.equals("deadline")) {
+            String splitBy = " /by ";
+            int firstByIndex = userInputSecondHalf.indexOf(splitBy); // Handle exception if missing; -1
+
+            checkForDeadlineExceptions(command, userInput, splitBy, firstByIndex);
+
+            String name = userInputSecondHalf.substring(0, firstByIndex);
+            String dueBy = userInputSecondHalf.substring(firstByIndex + splitBy.length());
+
+            task = new Deadline(name, dueBy);
+        } else { // Event
+            String splitFrom = " /from ";
+            String splitTo = " /to ";
+
+            int firstFromIndex = userInputSecondHalf.indexOf(splitFrom);
+            int firstToIndex = userInputSecondHalf.indexOf(splitTo);
+
+            checkForEventExceptions(command, userInput, splitFrom, firstFromIndex, firstToIndex);
+
+            String name = userInputSecondHalf.substring(0, firstFromIndex);
+            String start = userInputSecondHalf.substring(firstFromIndex + splitFrom.length(), firstToIndex);
+            String end = userInputSecondHalf.substring(firstToIndex + splitTo.length());
+
+            task = new Event(name, start, end);
+        }
+
+        return task;
+    }
+
+    public boolean getEndProgram() {
+        return this.endProgram;
+    }
+
+    public String handleBye(int firstSpaceIndex, String command) throws ZellException {
+        checkIfCommandHasSpaces(command, firstSpaceIndex);
+        this.endProgram = true;
+        return "Goodbye. Hope to see you again soon!";
+    }
+
+    public String handleList(int firstSpaceIndex, String command, TaskList taskList) throws ZellException {
+        checkIfCommandHasSpaces(command, firstSpaceIndex);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Currently you have added this tasks to your list:\n");
+        stringBuilder.append(taskList.listAllTasks());
+
+        return stringBuilder.toString();
+    }
+
+    public String handleMarkOrUnMark(String command, String userInput, int firstSpaceIndex, TaskList taskList)
+    throws ZellException{
+        int index = parseIndexForMarkOrUnMark(command, userInput, firstSpaceIndex, taskList);
+
+        Task currentTask = taskList.getTask(index);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (command.equals("mark")) {
+            taskList.markTaskAsDone(index);
+            stringBuilder.append("Nice! I've marked this task as done:\n ");
+        } else {
+            taskList.markTaskAsNotDone(index);
+            stringBuilder.append("OK, I've marked this task as not done yet:\n ");
+        }
+
+        stringBuilder.append(currentTask);
+
+        return stringBuilder.toString();
+    }
+
+    public int parseIndexForMarkOrUnMark(String command, String userInput, int firstSpaceIndex, TaskList taskList)
+    throws ZellException {
+        handleNoSpacesForMarkOrUnMark(command, firstSpaceIndex);
+
+        String indexInStringForm = userInput.substring(firstSpaceIndex + 1);
+
+        int index;
+
+        // Throw exception if a non-number was provided
+        try {
+            index = Integer.parseInt(indexInStringForm);
+        } catch (NumberFormatException e) {
+            String formatMessage = String.format("%s is not a number, you should indicate a number from the list to %s." +
+                    "\n For example: mark 2", indexInStringForm, command);
+            throw new ZellException(formatMessage);
+        }
+
+        handleInvalidTaskNumber(index, taskList);
+
+        return index;
+    }
+
+    public void checkIfCommandHasSpaces(String command, int firstSpaceIndex) throws ZellException{
+        if (firstSpaceIndex != -1) {
+            String formatMessage = String.format("%s should not have anything after.\nFor example:\n%s", command, command);
+            throw new ZellException(formatMessage);
+        }
+    }
+
+    public void handleNoSpacesForMarkOrUnMark(String command, int firstSpaceIndex) throws ZellException {
+        if (firstSpaceIndex == -1) {
+            String formatMessage = String.format("%s should have a number to indicate which one to %s.\nFor example:\nmark 2", command, command);
+            throw new ZellException(formatMessage);
+        }
+    }
+
+    public void handleInvalidTaskNumber(int index, TaskList taskList) throws ZellException {
+        if (index > taskList.numberOfTask()) {
+            String formatMessage = String.format("Task %d does not exist, please indicate a task number from 1 to %d", index, taskList.numberOfTask());
+            throw new ZellException(formatMessage);
+        }
+    }
+
+    public void handleNoSpacesForTask(String command, int firstSpaceIndex) throws ZellException {
+        if (firstSpaceIndex == -1) {
+            String formatMessage;
+
+            if (command.equals("todo")) {
+                formatMessage = String.format("%s should include a thing to do.\nFor example:\n%s read books", command, command);
+            } else if (command.equals("deadline")) {
+                formatMessage = String.format("%s should include a thing to do.\nFor example:\n%s books /by  Sunday", command, command);
+            } else {
+                formatMessage = String.format("%s should include a thing to do.\nFor example:\n%s books /from Mon 2pm /to 4pm", command, command);
+            }
+
+            throw new ZellException(formatMessage);
+        }
+    }
+
+    public void checkForDeadlineExceptions(String command, String userInput, String splitBy, int firstByIndex) throws ZellException {
+        // Handle missing deadline name
+        if (userInput.contains(splitBy) && firstByIndex == - 1) {
+            String formatMessage = String.format("%s should include a name.\nFor example:\n" +
+                    "%s project meeting /by Mon 2pm", command, command);
+            throw new ZellException(formatMessage);
+        }
+
+        if (firstByIndex == - 1) {
+            String formatMessage = String.format("%s should include a dateline by using /by.\nFor example:\n" +
+                    "%s read books /by Sunday", command, command);
+            throw new ZellException(formatMessage);
+        }
+    }
+
+    public void checkForEventExceptions(String command, String userInput, String splitFrom, int firstFromIndex, int firstToIndex)
+            throws ZellException {
+        // Handle if no name for event was provided. event /from today /to tmr
+        if (userInput.contains(splitFrom) && firstFromIndex == - 1) {
+            String formatMessage = String.format("%s should include a name.\nFor example:\n" +
+                    "%s project meeting /from Mon 2pm /to 4pm", command, command);
+            throw new ZellException(formatMessage);
+        }
+
+        if (firstFromIndex == - 1) {
+            String formatMessage = String.format("%s should include a start by using /from.\nFor example:\n" +
+                    "%s project meeting /from Mon 2pm /to 4pm", command, command);
+            throw new ZellException(formatMessage);
+        }
+
+        if (firstToIndex == - 1) {
+            String formatMessage = String.format("%s should include a end by using /to.\nFor example:\n" +
+                    "%s project meeting /from Mon 2pm /to 4pm", command, command);
+            throw new ZellException(formatMessage);
+        }
+
+        // Handle exception if /to comes before /from
+        if (firstFromIndex > firstToIndex) {
+            String formatMessage = String.format("For %s /from should come before /to.\nFor example:\n" +
+                    "%s project meeting /from Mon 2pm /to 4pm", command, command);
+            throw new ZellException(formatMessage);
+        }
+
+        if (firstFromIndex + splitFrom.length() > firstToIndex) {
+            String formatMessage = String.format("For %s please include a start date/time using /from.\nFor example:\n" +
+                    "%s project meeting /from Mon 2pm /to 4pm", command, command);
+            throw new ZellException(formatMessage);
+        }
+    }
+}
