b4606f096fd94c3dc689590913af0f58848949b9
reineoeq <e1354688@u.nus.edu>
2025-08-19 14:21:25 +0800

    Level 2: Add, List

--- src/main/java/Rainy.java
+++ src/main/java/Rainy.java
@@ -4,18 +4,32 @@ public class Rainy {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = "____________________________________________________________\n";
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
+        /** intro**/
         System.out.println(line
                 + "Hello! I'm Rainy\n"
                 + "What can I do for you?\n"
                 + line);
+
         while (true) {
             String input = sc.nextLine();
 
             if (input.equals("bye")) {
                 System.out.println(line + " Bye. Hope to see you again soon!\n" + line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(line);
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(line + " added: " + input +"\n" + line);
             }
-            System.out.println(line + input + "\n" + line);
         }
         sc.close();
     }
