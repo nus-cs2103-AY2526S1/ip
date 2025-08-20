975cb47a8c9ee3f6b331e02ff11d470db9bc5bbd
limjimin-nus <e1356978@u.nus.edu>
2025-08-20 14:49:51 +0800

    Echo

--- src/main/java/Jimbot.java
+++ src/main/java/Jimbot.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class Jimbot {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                        "Hello! I'm Jimbot\n" +
-                        "What can I do for you?\n" +
-                        "____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+        String greet = "   ____________________________________________________________\n" +
+                        "   Hello! I'm Jimbot.\n" +
+                        "   What can I do for you?\n" +
+                        "   ____________________________________________________________";
+        System.out.println(greet);
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine().trim();
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("   ____________________________________________________________\n" +
+                        "   Bye. Hope to see you again soon!\n" +
+                        "   ____________________________________________________________");
+                break;
+            } else {
+                System.out.println("   ____________________________________________________________\n" +
+                "   " + userInput + "\n" +
+                "   ____________________________________________________________");
+            }
+        }
+        scanner.close();
     }
 }
