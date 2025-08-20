677a25cd0317f51c6cfbbd6663140be75fd7007f
Nguyen Quang Minh <minhmxc@gmail.com>
2025-08-20 16:03:02 +0800

    Implement Add and List command

--- src/main/java/MinhGPT.java
+++ src/main/java/MinhGPT.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MinhGPT {
+  private static void printDivider() {
+    System.out.println("------------------------------------------");
+  }
+
+  private static void printPadding(int size) {
+    String str = "";
+    for (int i = 0; i < size; i++) {
+      str += "\n";
+    }
+    System.out.print(str);
+  }
+
   private static void printStartupMessage() {
     String logo = """
           ███    ███ ██ ███    ██ ██   ██  ██████  ██████  ████████
@@ -9,30 +22,49 @@ public class MinhGPT {
           ██  ██  ██ ██ ██  ██ ██ ██   ██ ██    ██ ██         ██
           ██      ██ ██ ██   ████ ██   ██  ██████  ██         ██
         """;
-
-    System.out.println("\n\n" + logo + "\n");
-    System.out.println("Hello! I'm MinhGPT.\nWhat can I do for you?\n");
+    printPadding(2);
+    System.out.print(logo);
+    printPadding(2);
+    System.out.print("Hello! I'm MinhGPT.\nWhat can I do for you?");
+    printPadding(2);
+    printDivider();
   }
 
   private static void printExitMessage() {
-    System.out.println("\nBye. Hope to see you again soon!");
+    System.out.println("Bye. Hope to see you again soon!");
+  }
+
+  private static void program() {
+    String cmd = "";
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<String> list = new ArrayList<>();
+
+    while (true) {
+      System.out.print("Your input: ");
+      cmd = scanner.nextLine();
+      printPadding(1);
+      switch (cmd) {
+        case "bye":
+          scanner.close();
+          return;
+        case "list":
+          for (int i = 0; i < list.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, list.get(i)));
+          }
+          break;
+        default:
+          list.add(cmd);
+          System.out.println("Added: " + cmd);
+          break;
+      }
+      printDivider();
+    }
   }
 
   public static void main(String[] args) {
     printStartupMessage();
 
-    String cmd = "";
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      System.out.print("Your input: ");
-      cmd = scanner.next();
-      if (cmd.equals("bye")) {
-        break;
-      } else {
-        System.out.println("\n" + cmd + "\n\n");
-      }
-    }
-    scanner.close();
+    program();
 
     printExitMessage();
   }
