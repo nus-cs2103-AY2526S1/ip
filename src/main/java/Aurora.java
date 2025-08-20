bb3de5fd28df8d59ab10bff3c8c8a24381257609
Dylan <152871121+bluemayo@users.noreply.github.com>
2025-08-20 16:51:19 +0800

    Add function to list tasks

--- src/main/java/Aurora.java
+++ src/main/java/Aurora.java
@@ -1,4 +1,6 @@
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Aurora {
 
@@ -7,23 +9,44 @@ public class Aurora {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        String[] list = new String[100];
+        List<String> list = new ArrayList<String>();
 
         speak(INTRO);
-
-        String input = scanner.nextLine();
-
-        while (!input.equals("bye")) {
-            speak(input);
-            input = scanner.nextLine();
-        }
-
+        loop(scanner, list);
         speak(OUTRO);
         scanner.close();
     }
 
     public static void speak(String content) {
         System.out.println("Aurora: " + content);
+    }
+
+    public static void loop(Scanner s, List<String> list) {
+        String input = s.nextLine();
+
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                list(list);
+            } else {
+                add(input, list);
+            }
+            input = s.nextLine();
+        }
+    }
+
+    public static void add(String content, List<String> list) {
+        list.add(content);
+        System.out.println("Added: " + content);
+    }
+
+    public static void list(List<String> list) {
+        if (list.isEmpty()) {
+            speak("Your list is empty.");
+            return;
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.printf("%d. %s\n", i + 1, list.get(i));
+        }
     }
 }
