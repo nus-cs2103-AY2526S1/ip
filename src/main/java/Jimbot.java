8d8f68d2a20f5c55d115d2aa9e7790058abd872a
limjimin-nus <e1356978@u.nus.edu>
2025-08-20 21:26:35 +0800

    updated automated text ui testing

--- src/main/java/Jimbot.java
+++ src/main/java/Jimbot.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Jimbot {
     public static void main(String[] args) {
-        String greet = "   ____________________________________________________________\n" +
-                        "    Hello! I'm Jimbot! \n     (^з^)-☆\n" +
-                        "    What can I do for you?\n" +
-                        "   ____________________________________________________________";
+        String greet = """
+                   ____________________________________________________________
+                    Hello! I'm Jimbot!
+                     (^з^)-☆
+                    What can I do for you?
+                   ____________________________________________________________
+                """;
         System.out.println(greet);
         Scanner scanner = new Scanner(System.in);
         String userInput;
@@ -14,10 +17,13 @@ public class Jimbot {
 
         while (true) {
             userInput = scanner.nextLine().trim();
-            if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("   ____________________________________________________________\n" +
-                        "    Bye! Hope to see you again soon!\n    (^O^)／ \n" +
-                        "   ____________________________________________________________");
+            if (userInput.toLowerCase().contains("bye")) {
+                System.out.println("""
+                           ____________________________________________________________
+                            Bye! Hope to see you again soon!
+                            (^O^)／
+                           ____________________________________________________________
+                        """);
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
                 String list = "    Here are the tasks in your list:";
@@ -34,27 +40,26 @@ public class Jimbot {
                     System.out.println("   ____________________________________________________________\n" +
                             "    Nice! I've marked this task as done:\n" +
                             "       " + userList[index] + "\n" +
-                            "     ♪ ｖ（＾＿＾ｖ）♪ ♪（ｖ＾＿＾）ｖ ♪ \n" +
+                            "     ♪ ｖ（＾＿＾ｖ）♪ ♪（ｖ＾＿＾）ｖ ♪\n" +
                             "   ____________________________________________________________");
                 } else {
-                    System.out.println("Invalid task number! \n (╥﹏╥)");
+                    System.out.println("Invalid task number!\n (╥﹏╥)");
                 }
             } else if (userInput.startsWith("unmark")) {
                 int index = Integer.parseInt(userInput.split(" ")[1]) - 1;
                 if (index >= 0 && index < count) {
                     userList[index].markAsUndone();
                     System.out.println("   ____________________________________________________________\n" +
-                            "    (｀_´)ゞ -OK, I've marked this task as not done yet: \n" +
+                            "    (｀_´)ゞ -OK, I've marked this task as not done yet:\n" +
                             "       " + userList[index] + "\n" +
                             "   ____________________________________________________________");
                 } else {
-                    System.out.println("Invalid task number! \n (╥﹏╥)");
+                    System.out.println("Invalid task number!\n (╥﹏╥)");
                 }
             } else if (userInput.startsWith("deadline")) {
 
-
                 if (!userInput.contains("/by")) {
-                    System.out.println("Invalid deadline format. \n (╯°□°）╯︵ ┻━┻");
+                    System.out.println("Invalid deadline format!\n (╯°□°）╯︵ ┻━┻");
                 } else {
                     String[] deadline = userInput.substring(9)
                             .trim()
@@ -63,11 +68,11 @@ public class Jimbot {
                     String by = deadline[1].trim();
 
                     if (count >= 100) {
-                        System.out.println("Too many tasks! \n ＞_＜");
+                        System.out.println("Too many tasks!\n ＞_＜");
                     } else {
                         userList[count] = new Deadline(description, by);
                         System.out.println("   ____________________________________________________________\n" +
-                                "    Got it. I've added this task: \n" +
+                                "    Got it. I've added this task:\n" +
                                 "       " + userList[count++] + "\n" +
                                 "    Now you have " + count + " tasks in the list.\n" +
                                 "   ____________________________________________________________");
@@ -76,7 +81,7 @@ public class Jimbot {
             } else if (userInput.startsWith("event")) {
 
                 if (!userInput.contains("/from") || !userInput.contains("/to")) {
-                    System.out.println("Invalid event format... \n ಥ_ಥ");
+                    System.out.println("Invalid event format...\n ಥ_ಥ");
                 } else {
                     String[] event = userInput.substring(6)
                             .trim()
@@ -91,11 +96,11 @@ public class Jimbot {
                     String to = timings[1].trim();
 
                     if (count >= 100) {
-                        System.out.println("Too many tasks! \n ＞_＜");
+                        System.out.println("Too many tasks!\n ＞_＜");
                     } else {
                         userList[count] = new Event(description, from, to);
                         System.out.println("   ____________________________________________________________\n" +
-                                "    Got it. I've added this task: \n" +
+                                "    Got it. I've added this task:\n" +
                                 "       " + userList[count++] + "\n" +
                                 "    Now you have " + count + " tasks in the list.\n" +
                                 "   ____________________________________________________________");
@@ -105,18 +110,18 @@ public class Jimbot {
                 String description = userInput.substring(5).trim();
 
                 if (count >= 100) {
-                    System.out.println("Too many tasks! \n ＞_＜");
+                    System.out.println("Too many tasks!\n ＞_＜");
                 } else {
                     userList[count] = new ToDo(description);
                     System.out.println("   ____________________________________________________________\n" +
-                            "    Got it. I've added this task: \n" +
+                            "    Got it. I've added this task:\n" +
                             "       " + userList[count++] + "\n" +
                             "    Now you have " + count + " tasks in the list.\n" +
                             "   ____________________________________________________________");
                 }
             } else {
                 System.out.println("   ____________________________________________________________\n" +
-                        "    " + userInput + "\n    ʕ •ᴥ•ʔ     ʕ•ᴥ• ʔ \n" +
+                        "    " + userInput + "\n    ʕ •ᴥ•ʔ     ʕ•ᴥ• ʔ\n" +
                         "   ____________________________________________________________");
             }
         }
