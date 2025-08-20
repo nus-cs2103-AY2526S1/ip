70062a928ff750a686b84f81d05c1c302f381064
Dylan <152871121+bluemayo@users.noreply.github.com>
2025-08-20 14:59:56 +0800

    Rename class Duke to Aurora

---
+++ src/main/java/Aurora.java
@@ -0,0 +1,7 @@
+public class Aurora {
+    public static void main(String[] args) {
+        String intro = "Hello! I'm Aurora.\nWhat can I do for you?\n";
+        String outro = "Bye. Hope to see you again soon!\n";
+        System.out.println(intro + outro);
+    }
+}
