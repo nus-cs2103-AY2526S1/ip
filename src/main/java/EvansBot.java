865011068d6b02a6c71fd8e8a60ccf5bf1b14ced
evanjw2 <evanongjw@gmail.com>
2025-08-20 17:05:41 +0800

    added Scanner and echo function

--- src/main/java/EvansBot.java
+++ src/main/java/EvansBot.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class EvansBot {
 
     public static void main(String[] args) {
         Greet greeter = new Greet("EvansBot");
         Exit exiter = new Exit();
+        Scanner scanner = new Scanner(System.in);
+        String input;
 
         greeter.greet();
-        exiter.sayBye();
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                exiter.sayBye();
+                break;
+            }
+            else {
+                System.out.println("############################################################");
+                System.out.println(input);
+                System.out.println("############################################################");
+            }
+        }
+        scanner.close();
     }
 }
