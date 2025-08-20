a53b85ce258910cc5ce3fa088351908540c990a3
Kineeeeee <e1375463@u.nus.edu>
2025-08-20 23:06:23 +0800

    Update Sora.java

--- src/main/java/Sora.java
+++ src/main/java/Sora.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Sora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] task = new String[100];
+        Task[] task = new Task[100];
         int count =0;
 
         String logo = "  ____                 \n"
@@ -18,17 +18,31 @@ public class Sora {
 
         while(true){
             String input = scanner.nextLine();
-            if(input.equals("bye")){
+            String[] part = input.split(" ");
+            if(part.length==1 && part[0].equals("bye")){
                 break;
             }
-            else if (input.equals("list")){
+            else if (part.length==1 && part[0].equals("list")){
+                System.out.println("Here are the task in your list:");
                 for (int i=0;i<count;i++){
-                    System.out.println((i+1) + ". " + task[i]);
+                    System.out.println((i+1) + ". " + task[i].toString());
                 }
+            }
+            else if (part.length==2 && part[0].equals("mark")) {
+                Integer x = Integer.parseInt(part[1]);
+                task[x-1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(task[x-1].toString());
+            }
+            else if (part.length==2 && part[0].equals("unmark")) {
+                Integer x = Integer.parseInt(part[1]);
+                task[x-1].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(task[x-1].toString());
             }
             else{
                 System.out.println("added: " + input);
-                task[count]=input;
+                task[count]=new Task(input);
                 count++;
             }
         }
