d26852a517c5fb25e4083c8af755bba6dd48e65c
Dylan <152871121+bluemayo@users.noreply.github.com>
2025-08-20 15:18:03 +0800

    Aurora: echo user input

--- src/main/java/Aurora.java
+++ src/main/java/Aurora.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Aurora {
     public static void main(String[] args) {
-        String intro = "Hello! I'm Aurora.\nWhat can I do for you?\n";
-        String outro = "Bye. Hope to see you again soon!\n";
-        System.out.println(intro + outro);
+        Scanner scanner = new Scanner(System.in);
+        String intro = "Aurora: Hello! I'm Aurora.\nAurora: What can I do for you?";
+        String outro = "Aurora: Bye. Hope to see you again soon!";
+
+        System.out.println(intro);
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println("Aurora: " + input);
+            input = scanner.nextLine();
+        }
+
+        System.out.println(outro);
     }
 }
