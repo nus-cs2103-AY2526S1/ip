b1965973130dc1be722cafefa8f497a487396d00
limjimin-nus <e1356978@u.nus.edu>
2025-08-20 15:21:25 +0800

    Add, List

--- src/main/java/Jimbot.java
+++ src/main/java/Jimbot.java
@@ -1,25 +1,39 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Jimbot {
     public static void main(String[] args) {
         String greet = "   ____________________________________________________________\n" +
-                        "   Hello! I'm Jimbot.\n" +
-                        "   What can I do for you?\n" +
+                        "    Hello! I'm Jimbot.\n" +
+                        "    What can I do for you?\n" +
                         "   ____________________________________________________________";
         System.out.println(greet);
         Scanner scanner = new Scanner(System.in);
         String userInput;
+        List<String> userList = new ArrayList<>();
+
 
         while (true) {
             userInput = scanner.nextLine().trim();
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("   ____________________________________________________________\n" +
-                        "   Bye. Hope to see you again soon!\n" +
+                        "    Bye. Hope to see you again soon!\n" +
                         "   ____________________________________________________________");
                 break;
-            } else {
+            } else if (userInput.equalsIgnoreCase("list")) {
+                int count = userList.size();
+                String list = "";
+                for (int i = 0; i < count; i++) {
+                    list += "    " + (i + 1) + ". " + userList.get(i) + "\n";
+                }
                 System.out.println("   ____________________________________________________________\n" +
-                "   " + userInput + "\n" +
+                        list +
+                        "   ____________________________________________________________");
+            } else {
+                userList.add(userInput);
+                System.out.println("   ____________________________________________________________\n" +
+                "    added: " + userInput + "\n" +
                 "   ____________________________________________________________");
             }
         }
