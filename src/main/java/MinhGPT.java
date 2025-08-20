114aa54e6519d53205071b73a0fea868b79ba259
Nguyen Quang Minh <minhmxc@gmail.com>
2025-08-20 15:20:51 +0800

    Add echo functionality

--- src/main/java/MinhGPT.java
+++ src/main/java/MinhGPT.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class MinhGPT {
-  public static void main(String[] args) {
+  private static void printStartupMessage() {
     String logo = """
           ███    ███ ██ ███    ██ ██   ██  ██████  ██████  ████████
           ████  ████ ██ ████   ██ ██   ██ ██       ██   ██    ██
@@ -10,6 +12,28 @@ public class MinhGPT {
 
     System.out.println("\n\n" + logo + "\n");
     System.out.println("Hello! I'm MinhGPT.\nWhat can I do for you?\n");
-    System.out.println("Bye. Hope to see you again soon!");
+  }
+
+  private static void printExitMessage() {
+    System.out.println("\nBye. Hope to see you again soon!");
+  }
+
+  public static void main(String[] args) {
+    printStartupMessage();
+
+    String cmd = "";
+    Scanner scanner = new Scanner(System.in);
+    while (true) {
+      System.out.print("Your input: ");
+      cmd = scanner.next();
+      if (cmd.equals("bye")) {
+        break;
+      } else {
+        System.out.println("\n" + cmd + "\n\n");
+      }
+    }
+    scanner.close();
+
+    printExitMessage();
   }
 }
