d21f3af8ab74e3b46ab02e8403f63fde77462a05
romidas99 <tkaihsien619@yahoo.com>
2025-08-21 19:52:33 +0800

    Add ability to mark as done

--- src/main/java/Romidas.java
+++ src/main/java/Romidas.java
@@ -2,9 +2,31 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Romidas {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setIsDone(boolean isDone) {
+            this.isDone = isDone;
+        }
+
+        public String getStatusIcon() {
+            return isDone ? "X": " ";
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> store = new ArrayList<>();
+        ArrayList<Task> store = new ArrayList<>();
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Romidas");
         System.out.println("What can I do for you?");
@@ -13,12 +35,26 @@ public class Romidas {
         while (!input.equalsIgnoreCase("bye")) {
             System.out.println("____________________________________________________________");
             if (input.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < store.size(); i++) {
-                    System.out.println(i + 1 + ". " + store.get(i));
+                    Task task = store.get(i);
+                    System.out.println(i + 1 + ".[" + task.getStatusIcon() + "] " + task.getDescription());
                 }
-            }
-            else{
-                store.add(input);
+            } else if (input.toLowerCase().startsWith("mark")) {
+                System.out.println("Nice! I've marked this task as done:");
+                int lastChar = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
+                Task task = store.get(lastChar);
+                task.setIsDone(true);
+                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+
+            } else if (input.toLowerCase().startsWith("unmark")) {
+                System.out.println("OK, I've marked this task as not done yet:");
+                int lastChar = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
+                Task task = store.get(lastChar);
+                task.setIsDone(false);
+                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+            } else{
+                store.add(new Task(input));
                 System.out.println("added: " + input);
             }
             System.out.println("____________________________________________________________");
