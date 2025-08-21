143d3e649359852d3a7ccb43911d11a5af262dec
itisnotjord <jordan25888@gmail.com>
2025-08-21 16:24:26 +0800

    Level-2

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class Penguin {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         System.out.println("Hello! I'm Penguin\nWhat can I do for you?\n");
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
             if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!\n");
                 break;
+            } else if (line.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i+1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = line;
+                taskCount++;
+                System.out.println("added: " + line);
             }
-            System.out.println(line);
         }
     }
 }
