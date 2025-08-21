1558210a45e39e544a8baa843dd9a0d38a4142c1
romidas99 <tkaihsien619@yahoo.com>
2025-08-21 15:51:49 +0800

    Implement add and list feature

--- src/main/java/Romidas.java
+++ src/main/java/Romidas.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Romidas {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> store = new ArrayList<>();
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Romidas");
         System.out.println("What can I do for you?");
@@ -10,7 +12,15 @@ public class Romidas {
         String input = scanner.nextLine();
         while (!input.equalsIgnoreCase("bye")) {
             System.out.println("____________________________________________________________");
-            System.out.println(input);
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < store.size(); i++) {
+                    System.out.println(i + 1 + ". " + store.get(i));
+                }
+            }
+            else{
+                store.add(input);
+                System.out.println("added: " + input);
+            }
             System.out.println("____________________________________________________________");
             input = scanner.nextLine();
 
