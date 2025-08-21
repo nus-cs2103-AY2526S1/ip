5b8de09adb0a30497db8ef6d020fb1c305f29dcd
jeffreylim24 <jeffreylimhms@gmail.com>
2025-08-21 11:45:39 +0800

    Level 0
    Rename chatbot to Mumbo
    
    Add greeting & farewell lines

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Mumbo User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Mumbo.java
@@ -0,0 +1,8 @@
+public class Mumbo {
+    public static void main(String[] args) {
+        String line = "_______________________________________\n";
+        String greeting = line + "Hello! I'm Mumbo!\nWhat can I do for you?\n" + line;
+        String farewell = "Bye. Hope to see you again soon!\n" + line;
+        System.out.println(greeting + farewell);
+    }
+}
