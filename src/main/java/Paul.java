8e4d6784be231816b1ce85b6e8114deb97311632
TeoShiYuan <194579565+TeoShiYuan@users.noreply.github.com>
2025-08-21 23:06:43 +0800

    Change Duke to Paul, added skeletal version

---
+++ src/main/java/Paul.java
@@ -0,0 +1,17 @@
+public class Paul {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String logo = " ____   _   _   _ _\n"
+                + "|  _ \\ / \\ | | | | |\n"
+                + "| |_) / _ \\| | | | |\n"
+                + "|  __/ ___ \\ |_| | |___\n"
+                + "|_| /_/   \\_\\___/|_____|\n";
+        System.out.println(line);
+        System.out.println("Hello I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
