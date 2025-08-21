7752deb86962a0666dc0de0958d8382fad24fac9
evanjw2 <evanongjw@gmail.com>
2025-08-21 13:04:17 +0800

    small changes to text-ui-test

--- src/main/java/InvalidTodoException.java
+++ src/main/java/InvalidTodoException.java
@@ -1,5 +1,5 @@
 public class InvalidTodoException extends EvansBotException{
     public InvalidTodoException() {
-        super(" A todo needs a description. Please add the description after typing todo. for example; todo borrow a book");
+        super("A todo needs a description. Please add the description after typing todo. for example; todo borrow a book");
     }
 }
