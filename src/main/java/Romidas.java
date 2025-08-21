3301a5d180a8ad6a538e0e481b5dfd6e07221141
romidas99 <tkaihsien619@yahoo.com>
2025-08-21 14:27:33 +0800

    Add echo

--- src/main/java/Romidas.java
+++ src/main/java/Romidas.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
+
 public class Romidas {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Romidas");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+        String input = scanner.nextLine();
+        while (!input.equalsIgnoreCase("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            input = scanner.nextLine();
+
+        }
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
+
     }
 }
