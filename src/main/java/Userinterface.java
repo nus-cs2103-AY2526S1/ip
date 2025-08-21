66b412ef0fab2d1868fc15c8f58fee72ff2cf826
vanphuc <e1375522@u.nus.edu>
2025-08-21 10:23:47 +0800

    Add the methods for add and list action in Ui

--- src/main/java/Userinterface.java
+++ src/main/java/Userinterface.java
@@ -1,5 +1,11 @@
 public class Userinterface {
     private final String line = "____________________________________________________________";
+    private final String userinfo[];
+    private Integer count = 0;
+
+    public Userinterface() {
+        userinfo = new String[100];
+    }
 
     public void print(String input) {
         System.out.println(line);
@@ -18,5 +24,27 @@ public class Userinterface {
 
     public void echo(String input) {
         print(input);
+    }
+
+    public void addlist(String input) {
+        if (input.equals("list")) {
+            printlist();
+        }
+        else {
+            userinfo[count] = input;
+            print("added: " + input);
+            count++;
+        }
+    }
+
+    public void printlist() {
+        String output = "";
+        for (int i = 0; i < count; i++) {
+            String num = Integer.toString(i+1);
+            output += num + ". " + userinfo[i] + "\n";
+        }
+        System.out.println(line);
+        System.out.print(output);
+        System.out.println(line);
     }
 }
