fc32af3d6a3ce4cd4c4c1117ea3a1d34888c7a22
Gokul <gokulfan123@gmail.com>
2025-08-21 21:41:18 +0800

    Rename main class and add greeting

---
+++ src/main/java/GoksChat.java
@@ -0,0 +1,10 @@
+public class GoksChat {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("What's up, what's up! I'm GoksChat");
+        System.out.println("How can I assist you today?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Good Day! Hope to see you again!");
+        System.out.println("____________________________________________________________");
+    }
+}
