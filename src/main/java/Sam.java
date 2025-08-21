429db04532879c00e99b826f46789dda53df9375
Chong Rui <chongrui2003@gmail.com>
2025-08-21 16:36:59 +0800

    Update echoing of user commands

--- src/main/java/Sam.java
+++ src/main/java/Sam.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
 public class Sam {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String intro = "____________________________________________________________\n"
                 + " Hello! I'm Sam\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n";
+                //+ " Bye. Hope to see you again soon!\n"
+                //+ "____________________________________________________________\n";
         System.out.println(intro);
+
+        while (true) {
+            String input = sc.nextLine();  // <-- this is where user types
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break; // exit the loop
+            }
+
+            // Echo the input
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        sc.close();
     }
 }
