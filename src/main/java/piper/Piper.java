c8f66c5d460a55dbbcf10f79afc8f7897d0914bb
Ong Yi Xin <yixinong34@gmail.com>
2025-08-22 00:53:28 +0800

    Update while loop logic

--- src/main/java/piper/Piper.java
+++ src/main/java/piper/Piper.java
@@ -18,62 +18,70 @@ public class Piper {
 
         while (!exit) { // user is active
             String userInput = ui.read();
-            if (userInput.equals("bye")) { // user is inactive
-                ui.farewellUser();
-                exit = true;
-            } else if (userInput.equals("list")) {
-                // list all tasks
-                ui.displayTasks(tasks);
-            } else if (userInput.startsWith("mark ") || userInput.startsWith("unmark ")) {
-                // mark task as done or undone
+            String[] substrings = userInput.split(" ", 2);
+            String cmd = substrings[0];
 
-                String[] substrings = userInput.split(" ", 2); // split user input into action substring nd task index substring
-                String actionSubstring = substrings[0];
-                String numberSubstring = substrings[1];
-                int taskNumber = Integer.parseInt(numberSubstring);
-                int index = taskNumber - 1; // task list starts from index 1 but array list starts from index 0
-
-                Task task = tasks.getTask(index);
-                if (actionSubstring.equals("mark")) {
-                    task.markDone();
-                } else if (actionSubstring.equals("unmark")) {
-                    task.markUndone();
+            if (substrings.length < 2) {
+                if (userInput.equals("bye")) { // user is inactive
+                    ui.farewellUser();
+                    exit = true;
+                } else if (userInput.equals("list")) {
+                    // list all tasks
+                    ui.displayTasks(tasks);
                 }
-
-                ui.showTaskStatus(task);
             } else {
-                // add new task
-
-                String[] substrings = userInput.split(" ", 2);
-                String taskType = substrings[0];
                 String remainingSubstring = substrings[1];
-                Task task = null;
 
-                switch (taskType) {
-                    case "todo":
-                        task = new Todo(remainingSubstring);
-                        break;
-                    case "deadline": {
-                        String[] descriptionAndBy = remainingSubstring.split("/by ", 2);
-                        String description = descriptionAndBy[0];
-                        String by = descriptionAndBy[1];
-                        task = new Deadline(description, by);
-                        break;
+                if (cmd.equals("mark") || cmd.equals("unmark")) {
+                    // mark task as done or undone
+
+                    int taskNumber = Integer.parseInt(remainingSubstring);
+                    int index = taskNumber - 1; // task list starts from index 1 but array list starts from index 0
+
+                    Task task = tasks.getTask(index);
+
+                    switch (cmd) {
+                        case "mark":
+                            task.markDone();
+                            break;
+                        case "unmark":
+                            task.markUndone();
+                            break;
                     }
-                    case "event": {
-                        String[] descriptionAndFrom = remainingSubstring.split("/from ", 2);
-                        String description = descriptionAndFrom[0];
-                        String[] fromAndTo = descriptionAndFrom[1].split("/to ", 2);
-                        String from = fromAndTo[0];
-                        String to = fromAndTo[1];
-                        task = new Event(description, from, to);
-                        break;
+
+                    ui.showTaskStatus(task);
+                } else if (cmd.equals("todo") || cmd.equals("deadline") || cmd.equals("event")) {
+                    // add new task
+                    Task task = null;
+
+                    switch (cmd) {
+                        case "todo":
+                            task = new Todo(remainingSubstring);
+                            break;
+                        case "deadline": {
+                            String[] descriptionAndBy = remainingSubstring.split("/by ", 2);
+                            String description = descriptionAndBy[0];
+                            String by = descriptionAndBy[1];
+                            task = new Deadline(description, by);
+                            break;
+                        }
+                        case "event": {
+                            String[] descriptionAndFrom = remainingSubstring.split("/from ", 2);
+                            String description = descriptionAndFrom[0];
+                            String[] fromAndTo = descriptionAndFrom[1].split("/to ", 2);
+                            String from = fromAndTo[0];
+                            String to = fromAndTo[1];
+                            task = new Event(description, from, to);
+                            break;
+                        }
                     }
+
+                    tasks.addTask(task);
+                    ui.showAddedTask(task);
+                    ui.getTasksSize(tasks);
+                } else {
+                    // error handling
                 }
-
-                tasks.addTask(task);
-                ui.showAddedTask(task);
-                ui.getTasksSize(tasks);
             }
         }
         ui.close();
