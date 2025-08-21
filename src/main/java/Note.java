17cca30101c199e2eaf04529a87199cc14df6f6e
enamourous <e1354400@u.nus.edu>
2025-08-21 18:33:04 +0800

    Implement Level-0: Rename, Greet, Exit

---
+++ src/main/java/Note.java
@@ -0,0 +1,17 @@
+public class Note {
+    public static void main(String[] args) {
+        String logo =
+                " _   _       _        \n"
+                        + "| \\ | | ___ | |_ ___  \n"
+                        + "|  \\| |/ _ \\| __/ _ \\ \n"
+                        + "| |\\  | (_) | || (_) |\n"
+                        + "|_| \\_|\\___/ \\__\\___/ \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Note");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
