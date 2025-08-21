d517c2a40471674e38327b0f74977b21f8db1186
enamourous <e1354400@u.nus.edu>
2025-08-21 18:59:57 +0800

    Implement Level-3: Mark and unmark tasks using Task class

--- src/main/java/Note.java
+++ src/main/java/Note.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Note {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         String logo =
                 " _   _       _        \n"
@@ -29,15 +29,33 @@ public class Note {
             } else if (input.equals("list")) {
                 if (tasks.isEmpty()) {
                     System.out.println(" No tasks yet.");
-                } else {
+                } System.out.println(" Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                        System.out.println(" " + (i + 1) + "." + tasks.get(i));
                     }
-                }
-            } else {
+            } else if (input.startsWith("mark ")) {
                 // Add input to tasks
-                tasks.add(input);
-                System.out.println(" added: " + input);
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (index >= 0 && index < tasks.size()) {
+                    tasks.get(index).markAsDone();
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + tasks.get(index));
+                } else {
+                    System.out.println(" Invalid task number!");
+                }
+            } else if (input.startsWith("unmark ")) {
+                    int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                    if (index >= 0 && index < tasks.size()) {
+                        tasks.get(index).markAsNotDone();
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("   " + tasks.get(index));
+                    } else {
+                        System.out.println(" Invalid task number!");
+                    }
+            } else {
+                Task t = new Task(input);
+                tasks.add(t);
+                System.out.println(" added: " + t);
             }
             System.out.println("____________________________________________________________");
         }
