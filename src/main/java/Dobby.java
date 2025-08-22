15be1e498d611606191e548e12b93356c7969c2e
Jessie <jcc537743@gmail.com>
2025-08-22 11:55:37 +0800

    Edit main: Add greeting message

---
+++ src/main/java/Dobby.java
@@ -0,0 +1,7 @@
+public class Dobby {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Dobby \n");
+        System.out.println("What can I do for you? \n");
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+}
