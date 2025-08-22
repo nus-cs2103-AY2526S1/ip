75e6e24def852b182b40abfd623a316b86dbc270
Lunardrake <abc1a51h@gmail.com>
2025-08-22 11:23:09 +0800

    Added add,list functionality

--- src/main/java/LunarBot.java
+++ src/main/java/LunarBot.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LunarBot {
@@ -13,14 +15,27 @@ public class LunarBot {
 
     public static void echo() {
         Scanner sc = new Scanner(System.in);
+        List<String> history = new ArrayList<>();
 
         while (true) {
             System.out.print("Input: ");
             String input = sc.nextLine();
             if (input.equals("bye")) {
+                // exit
                 break;
             }
-            System.out.println(input);
+            else if (input.equals("list")) {
+                // print history
+                System.out.println("Printing history!");
+                for (int i = 1; i < history.size() + 1; i++) {
+                    System.out.println(i + ": " + history.get(i-1));
+                }
+            }
+            else {
+                System.out.println("added: " + input);
+            }
+            // add to history
+            history.add(input);
             System.out.println(LINE);
         }
     }
