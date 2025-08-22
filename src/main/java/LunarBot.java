2b3c11f8807310193566082d4eeaba60a86d0658
Lunardrakey <91524028+Lunardrakey@users.noreply.github.com>
2025-08-22 10:52:48 +0800

    Rename main class and add greeting

---
+++ src/main/java/LunarBot.java
@@ -0,0 +1,13 @@
+public class LunarBot {
+    public static void main(String[] args) {
+        String logo =   "  _                             ____        _   \n" +
+                        " | |                           |  _ \\      | |  \n" +
+                        " | |    _   _ _ __   __ _ _ __ | |_) | ___ | |_ \n" +
+                        " | |   | | | | '_ \\ / _` | '__||  _ < / _ \\| __|\n" +
+                        " | |___| |_| | | | | (_| | |   | |_) | (_) | |_ \n" +
+                        " |______\\__,_|_| |_|\\__,_|_|   |____/ \\___/ \\__|\n\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Nice to meet you!\n");
+        System.out.println("Hope to see you soon!\n");
+    }
+}
