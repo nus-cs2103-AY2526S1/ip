6dde8c4bffd43eeb4fa07ce67a2ae684965cdb7d
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-08-23 00:11:07 +0800

    Add JUnit test files and classes

---
+++ src/test/java/lynx/command/LynxCommandTest.java
@@ -0,0 +1,10 @@
+package lynx.command;
+
+import lynx.exception.LynxException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LynxCommandTest {
+
+}
