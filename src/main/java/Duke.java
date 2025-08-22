e3db8c7d68530a6cede60caaa66839446947efa8
x3zapper <142211386+x3zapper@users.noreply.github.com>
2025-08-22 14:22:46 +0800

    Update to Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,37 @@
-public class Duke {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm Crysis Heir Activity Sentre Hepdesk (CHASH).");
-        System.out.println("What can I do for you?");
+//Imports
+import java.util.Scanner;
 
-        System.out.println("Bye. Hope to see you again soon!");
+public class Duke {
+    private static final String LINESEP = "____________________________________________________________";
+    private static final String EXIT = "bye";
+
+    //Handles printing format
+    private static void chatPrint(String txt) {
+        //todo: handle multiline indents for all inputs
+        System.out.println(String.format(
+            "\t%s\n\t%s\n\t%s",
+            Duke.LINESEP,
+            txt,
+            Duke.LINESEP
+        ));
+    }
+
+    //Entrypoint
+    public static void main(String[] args) {
+        //Entry message
+        chatPrint("Hello! I'm Crysis Heir Activity Sentre Hepdesk (CHASH)." + 
+            "\n\t" + "What can I do for you?");
+
+        //Work loop
+        Scanner scanStdin = new Scanner(System.in);
+        while (true) {
+            String userCmd = scanStdin.nextLine();
+
+            if (userCmd.equals(Duke.EXIT)) {
+                chatPrint("Bye. Hope to see you again soon!");
+                break;
+            }
+            chatPrint(userCmd);
+        }
     }
 }
