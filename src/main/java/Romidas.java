dd6be4ffb1b15537921f99250e6004906e53d4a7
romidas99 <tkaihsien619@yahoo.com>
2025-08-22 10:04:59 +0800

    Add todo, events, deadline task types

--- src/main/java/Romidas.java
+++ src/main/java/Romidas.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Romidas {
-    public static class Task {
+    public abstract static class Task {
         protected String description;
         protected boolean isDone;
 
@@ -20,7 +20,48 @@ public class Romidas {
         }
 
         public String getStatusIcon() {
-            return isDone ? "X": " ";
+            return isDone ? "[X]": "[ ]";
+        }
+
+        public abstract String getStatus();
+
+        @Override
+        public String toString() {
+            return this.getStatus() + this.getStatusIcon() + " " + this.getDescription();
+        }
+    }
+
+    public static class TodoTask extends Task {
+        public TodoTask(String description) {
+            super(description);
+        }
+
+        @Override
+        public String getStatus() {
+            return "[T]";
+        }
+
+    }
+
+    public static class DeadlineTask extends Task {
+        public DeadlineTask(String description) {
+            super(description);
+        }
+
+        @Override
+        public String getStatus() {
+            return "[D]";
+        }
+    }
+
+    public static class Event extends Task {
+        public Event(String description) {
+            super(description);
+        }
+
+        @Override
+        public String getStatus() {
+            return "[E]";
         }
     }
 
@@ -38,24 +79,41 @@ public class Romidas {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < store.size(); i++) {
                     Task task = store.get(i);
-                    System.out.println(i + 1 + ".[" + task.getStatusIcon() + "] " + task.getDescription());
+                    System.out.println(i + 1 + "." + task.toString());
                 }
             } else if (input.toLowerCase().startsWith("mark")) {
                 System.out.println("Nice! I've marked this task as done:");
-                int lastChar = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
-                Task task = store.get(lastChar);
+                int index = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
+                Task task = store.get(index);
                 task.setIsDone(true);
-                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+                System.out.println("  " + task.toString());
 
             } else if (input.toLowerCase().startsWith("unmark")) {
                 System.out.println("OK, I've marked this task as not done yet:");
-                int lastChar = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
-                Task task = store.get(lastChar);
+                int index = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
+                Task task = store.get(index);
                 task.setIsDone(false);
-                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+                System.out.println("  " + task.toString());
             } else{
-                store.add(new Task(input));
-                System.out.println("added: " + input);
+                System.out.println("Got it. I've added this task:");
+                Task task = null;
+                if (input.toLowerCase().startsWith("todo")) {
+                    String description = input.substring(5);
+                    task = new TodoTask(description);
+                } else if (input.toLowerCase().startsWith("deadline")) {
+                    String sub = input.substring(9);
+                    String[] parts = sub.split(" /by ");
+                    task = new DeadlineTask(parts[0] +  " (by: " + parts[1] + ")");
+                } else if (input.toLowerCase().startsWith("event")) {
+                    String sub = input.substring(6);
+                    String[] parts = input.split(" /from ");
+                    String fromAndTo = parts[1];
+                    String[] timeParts = fromAndTo.split(" /to ");
+                    task = new DeadlineTask(parts[0] +  " (from: " + timeParts[0] + " to: " +  timeParts[1] + ")");
+                }
+                store.add(task);
+                System.out.println("  " + task.toString());
+                System.out.println("Now you have " + store.size() + " tasks in your list.");
             }
             System.out.println("____________________________________________________________");
             input = scanner.nextLine();
