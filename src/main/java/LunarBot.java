b7914851ef831b05fcd032a9bec80a43ee13e00f
Lunardrake <abc1a51h@gmail.com>
2025-08-22 11:46:22 +0800

    Changed tasks to be tracked with a separate class

--- src/main/java/LunarBot.java
+++ src/main/java/LunarBot.java
@@ -2,6 +2,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+class Task {
+    String name;
+    Boolean completed;
+
+    Task(String name, Boolean completed) {
+        this.name = name;
+        this.completed = completed;
+    }
+
+    public void setCompeleted(Boolean bool) {
+        this.completed = bool;
+    }
+
+    public String print() {
+        return "[" + (this.completed ? "X" : " ") + "] " + this.name;
+    }
+}
+
 public class LunarBot {
     public static final String LINE = "__________________________________________";
     public static void main(String[] args) {
@@ -15,8 +33,7 @@ public class LunarBot {
 
     public static void echo() {
         Scanner sc = new Scanner(System.in);
-        List<String> history = new ArrayList<>();
-        List<Boolean> completed = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         while (true) {
             System.out.print("Input: ");
@@ -28,26 +45,36 @@ public class LunarBot {
             else if (input.equals("list")) {
                 // print history
                 System.out.println("Printing history!");
-                for (int i = 1; i < history.size() + 1; i++) {
-                    System.out.println(i + ": [" + (completed.get(i-1) ? "X" : " ") + "]" + history.get(i-1));
+                for (int i = 1; i < tasks.size() + 1; i++) {
+                    System.out.print(i + ": ");
+                    System.out.println(tasks.get(i-1).print());
                 }
             }
             else if (input.split(" ")[0].equals("mark") && input.split(" ").length > 1){
                 int index = Integer.valueOf(input.split(" ")[1]) - 1;
+                if (index < 0 || index > tasks.size()) {
+                    System.out.println("Operation not possible!");
+                    System.out.println(LINE);
+                    continue;
+                }
                 System.out.println("Okay, I'll mark that one off your list!");
-                System.out.println("[X] " + history.get(index));
-                completed.set(index, true);
+                tasks.get(index).setCompeleted(true);
+                System.out.println(tasks.get(index).print());
             }
             else if (input.split(" ")[0].equals("unmark") && input.split(" ").length > 1){
                 int index = Integer.valueOf(input.split(" ")[1]) - 1;
+                if (index < 0 || index > tasks.size()) {
+                    System.out.println("Operation not possible!");
+                    System.out.println(LINE);
+                    continue;
+                }
                 System.out.println("Okay, I'll unmark that!");
-                System.out.println("[ ] " + history.get(index));
-                completed.set(index, false);
+                tasks.get(index).setCompeleted(false);
+                System.out.println(tasks.get(index).print());
             }
             else {
                 System.out.println("added: " + input);
-                history.add(input);
-                completed.add(false);
+                tasks.add(new Task(input, false));
             }
             // add to history
             System.out.println(LINE);
