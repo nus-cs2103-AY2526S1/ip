7019a776dae84d69502631f1ebd5cfeb35173c3b
TeoShiYuan <194579565+TeoShiYuan@users.noreply.github.com>
2025-08-22 11:16:42 +0800

    Improves user inputs, and changed line and logo to constants

--- src/main/java/Paul.java
+++ src/main/java/Paul.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
 public class Paul {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        String logo = """
+    public static final String LINE = "____________________________________________________________";
+    public static final String LOGO = """
                  ____   _   _   _ _
                 |  _ \\ / \\ | | | | |
                 | |_) / _ \\| | | | |
@@ -11,26 +10,28 @@ public class Paul {
                 |_| /_/   \\_\\___/|_____|
                 """;
 
-        System.out.println(line);
-        System.out.println("Hello I'm\n" + logo);
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.println("Hello I'm\n" + LOGO);
         System.out.println("What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
 
         Scanner sc = new Scanner(System.in);
 
         while(true) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
 
-            if (input.equals("bye")) {
-                System.out.println(line);
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(LINE);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
+                System.out.println(LINE);
                 break;
             }
 
-            System.out.println(line);
+            System.out.println(LINE);
             System.out.println(input);
-            System.out.println(line);
+            System.out.println(LINE);
         }
+        sc.close();
     }
 }
