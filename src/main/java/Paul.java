f0b305cb120ef92c9c0210226acb3550784df4b1
TeoShiYuan <194579565+TeoShiYuan@users.noreply.github.com>
2025-08-22 11:08:02 +0800

    Added scanner, echos command by user (Level-1)

--- src/main/java/Paul.java
+++ src/main/java/Paul.java
@@ -1,17 +1,36 @@
+import java.util.Scanner;
+
 public class Paul {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
-        String logo = " ____   _   _   _ _\n"
-                + "|  _ \\ / \\ | | | | |\n"
-                + "| |_) / _ \\| | | | |\n"
-                + "|  __/ ___ \\ |_| | |___\n"
-                + "|_| /_/   \\_\\___/|_____|\n";
+        String logo = """
+                 ____   _   _   _ _
+                |  _ \\ / \\ | | | | |
+                | |_) / _ \\| | | | |
+                |  __/ ___ \\ |_| | |___
+                |_| /_/   \\_\\___/|_____|
+                """;
+
         System.out.println(line);
         System.out.println("Hello I'm\n" + logo);
         System.out.println("What can I do for you?");
         System.out.println(line);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
     }
 }
