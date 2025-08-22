694c4b56c3a5fccb90e61590d96fb7c866648545
Lunardrake <abc1a51h@gmail.com>
2025-08-22 11:05:57 +0800

    Rename main file, and add greeting

---
+++ src/main/java/LunarBot.java
@@ -0,0 +1,7 @@
+public class LunarBot {
+    public static void main(String[] args) {
+        System.out.println("Hello from LunarBot!\n__________________________________________\n");
+        System.out.println("Nice to meet you!\n");
+        System.out.println("Hope to see you soon!\n");
+    }
+}
