1e1d7dbed039cceeda1be7ddd71b3e61f8f69bc6
oysterboi <yanshengong@gmail.com>
2025-08-22 15:36:33 +0800

    Added scanner to read user input and echo accordingly

--- src/main/java/Clam.java
+++ src/main/java/Clam.java
@@ -1,20 +1,38 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Clam {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n");
+        System.out.println("    ____________________________________________________________");
+
         greet();
-        bye();
+
+        boolean end = false;
+        Scanner sc = new Scanner(System.in);
+        while(!end) {
+            String input = sc.nextLine();
+            System.out.println("    ____________________________________________________________");
+            if(Objects.equals(input, "bye")) {
+                end = true;
+                bye();
+                sc.close();
+            } else {
+                System.out.println("    " + input);
+                System.out.println("    ____________________________________________________________");
+            }
+        }
     }
 
     public static void greet() {
-        String greeting = "Hello! I'm Clam!\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n";
+        String greeting = "    Hello! I'm Clam!\n"
+                + "    What can I do for you?\n"
+                + "    ____________________________________________________________\n";
         System.out.println(greeting);
     }
 
     public static void bye() {
-        String bye = "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
+        String bye = "    Bye. Hope to see you again soon!\n"
+                + "    ____________________________________________________________\n";
         System.out.println(bye);
     }
 }
