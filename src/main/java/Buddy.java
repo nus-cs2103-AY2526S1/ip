85fbcc1e84709da18d680709b64c7f2c09e098ae
yuzongmin <yuzongmin@u.nus.edu>
2025-08-22 12:24:11 +0800

    Level-2: Add task storage and listing

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Buddy {
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int taskCount = 0;
+        
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Buddy");
         System.out.println(" What can I do for you?");
@@ -18,11 +21,19 @@ public class Buddy {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
-            
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
         
         scanner.close();
