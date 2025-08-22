8a364ea83f0700506dce4c5382afe7fc581a686c
limjimin-nus <e1356978@u.nus.edu>
2025-08-22 16:46:42 +0800

    bug fix

--- src/main/java/util/Helper.java
+++ src/main/java/util/Helper.java
@@ -1,13 +1,18 @@
+//helper class for useful fns
 package util;
 
 import exceptions.InvalidIndexException;
 
 public class Helper {
     public static int parseIndex(String input, String command, int taskCount) throws InvalidIndexException {
-        int index = Integer.parseInt(input
-                .substring(command.length())
-                .trim()) - 1;
-        if (index < 0 || index >= taskCount) throw new InvalidIndexException();
-        return index;
+        try {
+            int index = Integer.parseInt(input
+                    .substring(command.length())
+                    .trim()) - 1;
+            if (index < 0 || index >= taskCount) throw new InvalidIndexException();
+            return index;
+        } catch (NumberFormatException e) {
+            throw new InvalidIndexException();
+        }
     }
 }
