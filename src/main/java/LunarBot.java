e8cc14eb0f6e4fd94e79d3dc814d408231eb534b
Lunardrake <abc1a51h@gmail.com>
2025-08-22 11:37:07 +0800

    added mark,unmark functionality

--- src/main/java/LunarBot.java
+++ src/main/java/LunarBot.java
@@ -16,6 +16,7 @@ public class LunarBot {
     public static void echo() {
         Scanner sc = new Scanner(System.in);
         List<String> history = new ArrayList<>();
+        List<Boolean> completed = new ArrayList<>();
 
         while (true) {
             System.out.print("Input: ");
@@ -28,12 +29,25 @@ public class LunarBot {
                 // print history
                 System.out.println("Printing history!");
                 for (int i = 1; i < history.size() + 1; i++) {
-                    System.out.println(i + ": " + history.get(i-1));
+                    System.out.println(i + ": [" + (completed.get(i-1) ? "X" : " ") + "]" + history.get(i-1));
                 }
+            }
+            else if (input.split(" ")[0].equals("mark") && input.split(" ").length > 1){
+                int index = Integer.valueOf(input.split(" ")[1]) - 1;
+                System.out.println("Okay, I'll mark that one off your list!");
+                System.out.println("[X] " + history.get(index));
+                completed.set(index, true);
+            }
+            else if (input.split(" ")[0].equals("unmark") && input.split(" ").length > 1){
+                int index = Integer.valueOf(input.split(" ")[1]) - 1;
+                System.out.println("Okay, I'll unmark that!");
+                System.out.println("[ ] " + history.get(index));
+                completed.set(index, false);
             }
             else {
                 System.out.println("added: " + input);
                 history.add(input);
+                completed.add(false);
             }
             // add to history
             System.out.println(LINE);
