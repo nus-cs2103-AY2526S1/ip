ec9a49bc91dd1292f684627bf5bac50b88271da0
Lunardrake <abc1a51h@gmail.com>
2025-08-22 11:15:21 +0800

    Added echo feature

--- src/main/java/LunarBot.java
+++ src/main/java/LunarBot.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class LunarBot {
+    public static final String LINE = "__________________________________________";
     public static void main(String[] args) {
-        System.out.println("Hello from LunarBot!\n__________________________________________\n");
-        System.out.println("Nice to meet you!\n");
+        System.out.println("Hello from LunarBot!\n");
+        System.out.println("Nice to meet you! What can I do for you?\n" + LINE);
+        echo();
+        System.out.println(LINE);
         System.out.println("Hope to see you soon!\n");
+
+    }
+
+    public static void echo() {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Input: ");
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println(input);
+            System.out.println(LINE);
+        }
     }
 }
