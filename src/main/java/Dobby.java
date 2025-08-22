f0f08c6852c08bd51da95f873dc0014cc48bc718
Jessie <jcc537743@gmail.com>
2025-08-22 13:28:53 +0800

    Edit main: Add personality to Dobby

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class Dobby {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm Dobby \n");
         System.out.println("What can I do for you? \n");
-        System.out.println("Bye! Hope to see you again soon!");
+        boolean flag = true;
+
+        while (flag) {
+            System.out.print("> "); // prompt
+            String input = sc.nextLine(); // read user input
+            System.out.print("I hear that you have said: " + input + "\n");
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye! Hope to see you again soon!");
+                flag = false;
+            }
+        }
     }
 }
