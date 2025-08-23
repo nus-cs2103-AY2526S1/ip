c3c19f4ae98d3128e53911342192a96af76bf1a6
Jared <129586362+Jaredee123@users.noreply.github.com>
2025-08-24 02:00:00 +0800

    Add greeting

---
+++ src/main/java/SOFI.java
@@ -0,0 +1,8 @@
+public class SOFI {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm SOFI.\n" +
+                "What can I do for you?\n" +
+                "Bye. Hope to see you again soon!";
+        System.out.println(greet);
+    }
+}
