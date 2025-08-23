9ef836b67dd7a424060d0fb8e2c191142d2ac6c9
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-08-23 15:31:31 +0800

    Add javadoc comments for BobbyWasabiException Class

--- src/main/java/BobbyWasabi/Exceptions/BobbyWasabiException.java
+++ src/main/java/BobbyWasabi/Exceptions/BobbyWasabiException.java
@@ -1,6 +1,12 @@
 package BobbyWasabi.Exceptions;
 
 public class BobbyWasabiException extends Exception {
+
+    /**
+     * Constrcuts a new {@code BobbyWasabiException} with the specified detail message.
+     *
+     * @param msg The detail message explaining the reason for the exception.
+     */
     public BobbyWasabiException(String msg) {
         super(msg);
     }
