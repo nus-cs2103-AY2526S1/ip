002e35ff6d77e6451f74b6e15a5d48048d067fbd
Jared <129586362+Jaredee123@users.noreply.github.com>
2025-08-24 21:01:26 +0800

    Added Mark as Done feature

--- src/main/java/SOFI.java
+++ src/main/java/SOFI.java
@@ -1,8 +1,31 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
 
 public class SOFI {
+    static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        // Mark task as done
+        void markAsDone() {
+            isDone = true;
+        }
+
+        // Mark task as not done
+        void markAsNotDone() {
+            isDone = false;
+        }
+
+        // Get task status as [ ] or [X]
+        String getStatusIcon() {
+            return isDone ? "[X]" : "[ ]";
+        }
+    }
     public static void main(String[] args) {
         String greet = "____________________________________________________________\n" +
                 "Hello! I'm SOFI\n" +
@@ -12,7 +35,7 @@ public class SOFI {
 
         Scanner scanner = new Scanner(System.in);
         String userInput;
-        List<String> taskList = new ArrayList<>();
+        ArrayList<Task> taskList = new ArrayList<>();
 
         while (true) {
             userInput = scanner.nextLine();
@@ -25,13 +48,31 @@ public class SOFI {
 
             if (userInput.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskList.size(); i++) {
-                    System.out.println((i + 1) + ". " + taskList.get(i));
+                    Task task = taskList.get(i);
+                    System.out.println((i + 1) + "." + task.getStatusIcon() + " " + task.description);
                 }
                 System.out.println("____________________________________________________________");
-            }
-            else {
-                taskList.add(userInput);
+            } else if (userInput.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                Task task = taskList.get(taskNumber);
+                task.markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done:\n" + "   " + task.getStatusIcon() + " " +
+                        task.description);
+                System.out.println("____________________________________________________________");
+            } else if (userInput.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]) - 1;
+                Task task = taskList.get(taskNumber);
+                task.markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:\n" + "   " + task.getStatusIcon() + " " +
+                        task.description);
+                System.out.println("____________________________________________________________");
+            } else {
+                Task newTask = new Task (userInput);
+                taskList.add(newTask);
                 System.out.println("____________________________________________________________");
                 System.out.println("added: " + userInput);
                 System.out.println("____________________________________________________________");
