caef1d774b27bed11371a729947e4fc9dfe8ed04
tch1001 <tanchienhao@gmail.com>
2025-08-24 12:17:51 +0800

    level-3

--- src/main/java/PoopieMeow.java
+++ src/main/java/PoopieMeow.java
@@ -1,9 +1,10 @@
- import java.util.Scanner;
+import java.util.Scanner;
 public class PoopieMeow {
     public static void main(String[] args) {
         String input;
         Scanner sc = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
         
         System.out.println("____________________________________________________________");
@@ -21,16 +22,36 @@ public class PoopieMeow {
                 break;
             } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    System.out.println(" " + (i + 1) + ".[" + (isDone[i] ? "X" : " ") + "] " + tasks[i]);
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                if (index >= 0 && index < taskCount) {
+                    isDone[index] = true;
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   [X] " + tasks[index]);
+                    System.out.println("____________________________________________________________");
+                }
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                if (index >= 0 && index < taskCount) {
+                    isDone[index] = false;
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   [ ] " + tasks[index]);
+                    System.out.println("____________________________________________________________");
+                }
             } else {
                 tasks[taskCount] = input;
-                taskCount++;
+                isDone[taskCount] = false;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
+                taskCount++;
             }
         }
     }
