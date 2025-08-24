7a1095c595a815155e0db48d1fc22f7e5cb73c2c
Jared <129586362+Jaredee123@users.noreply.github.com>
2025-08-24 20:42:57 +0800

    Added list feature

--- src/main/java/SOFI.java
+++ src/main/java/SOFI.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SOFI {
     public static void main(String[] args) {
@@ -10,10 +12,10 @@ public class SOFI {
 
         Scanner scanner = new Scanner(System.in);
         String userInput;
+        List<String> taskList = new ArrayList<>();
 
         while (true) {
             userInput = scanner.nextLine();
-
             if (userInput.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
@@ -21,9 +23,19 @@ public class SOFI {
                 break;
             }
 
-            System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
+            if (userInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskList.size(); i++) {
+                    System.out.println((i + 1) + ". " + taskList.get(i));
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else {
+                taskList.add(userInput);
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + userInput);
+                System.out.println("____________________________________________________________");
+            }
         }
     }
 }
