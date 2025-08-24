9feec8292511601b2543cc2c85d42be672ab47c6
Jared <129586362+Jaredee123@users.noreply.github.com>
2025-08-24 18:58:22 +0800

    Added echo feature

--- src/main/java/SOFI.java
+++ src/main/java/SOFI.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class SOFI {
     public static void main(String[] args) {
-        String greet = "Hello! I'm SOFI.\n" +
+        String greet = "____________________________________________________________\n" +
+                "Hello! I'm SOFI\n" +
                 "What can I do for you?\n" +
-                "Bye. Hope to see you again soon!";
+                "____________________________________________________________";
         System.out.println(greet);
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
+
