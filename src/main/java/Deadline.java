58215aaa17a848843934a5468b1e79ee5d3347a6
Zisaac99 <zisaac2468@gmail.com>
2025-08-25 21:46:21 +0800

    Deadline.java: Modify to use DateOrTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,25 +1,25 @@
 public class Deadline extends Task{
-    private final String dueBy;
+    private final DateOrTime dueBy;
 
-    public Deadline(String name, String dueBy) {
+    public Deadline(String name, String dueBy) throws ZellException {
         super(name);
-        this.dueBy = dueBy;
+        this.dueBy = new DateOrTime(dueBy);
     }
 
-    public Deadline(String name, String dueBy, boolean isDone) {
+    public Deadline(String name, String dueBy, boolean isDone) throws ZellException {
         this(name, dueBy);
         setDone(isDone);
     }
 
     @Override
     public String taskToString() {
-        return String.format("%s | %b | %s | %s", "D", getDone(), getName(), this.dueBy);
+        return String.format("%s | %b | %s | %s", "D", getDone(), getName(), this.dueBy.originalFormat());
     }
 
 
     @Override
     public String toString() {
-        return String.format("[%s]%s (by: %s)", "D", super.toString(), this.dueBy);
+        return String.format("[%s]%s (by: %s)", "D", super.toString(), this.dueBy.toString());
     }
 }
 
