4db9dd844c1d03ee32361dd4147892844edbca8b
TeoShiYuan <194579565+TeoShiYuan@users.noreply.github.com>
2025-08-25 20:46:36 +0800

    Removes output lines, added PaulException

--- src/main/java/Paul.java
+++ src/main/java/Paul.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Paul {
-    public static final String LINE = "____________________________________________________________";
     public static final String LOGO = """
                  ____   _   _   _ _
                 |  _ \\ / \\ | | | | |
@@ -15,27 +14,21 @@ public class Paul {
         int index = Integer.parseInt(input.split(" ")[1]);
         list.mark(index);
 
-        System.out.println(LINE);
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(list.get(index));
-        System.out.println(LINE);
     }
 
     private static void unmarkTask(String input) {
         int index = Integer.parseInt(input.split(" ")[1]);
         list.unmark(index);
 
-        System.out.println(LINE);
         System.out.println("OK, I've marked this task as not done yet:");
         System.out.println(list.get(index));
-        System.out.println(LINE);
     }
 
     public static void main(String[] args) {
-        System.out.println(LINE);
         System.out.println("Hello I'm\n" + LOGO);
         System.out.println("What can I do for you?");
-        System.out.println(LINE);
 
         Scanner sc = new Scanner(System.in);
 
@@ -43,17 +36,13 @@ public class Paul {
             String input = sc.nextLine().trim();
 
             if (input.equalsIgnoreCase("bye")) {
-                System.out.println(LINE);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(LINE);
                 break;
             }
 
             if (input.equalsIgnoreCase("list")){
-                System.out.println(LINE);
                 System.out.println("Here are the tasks in your list:");
                 System.out.println(list);
-                System.out.println(LINE);
                 continue;
             }
 
@@ -74,17 +63,13 @@ public class Paul {
                 String[] str = input.substring(6).split(" /from | /to ");
                 list.add(new Event(str[0], str[1], str[2]));
             } else {
-                System.out.println(LINE);
-                System.out.println("Please input a valid task!");
-                System.out.println(LINE);
+                System.out.println("Invalid input!");
                 continue;
             }
 
-            System.out.println(LINE);
             System.out.println("Got it. I've added this task:");
             System.out.println(list.get(list.size()));
             System.out.println("Now you have " + list.size() + " tasks in the list.");
-            System.out.println(LINE);
         }
         sc.close();
     }
---
+++ src/main/java/PaulException.java
@@ -0,0 +1,5 @@
+public class PaulException extends Exception {
+    public PaulException(String message) {
+        super(message);
+    }
+}
