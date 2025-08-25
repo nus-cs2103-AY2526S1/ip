21ccc8eb4477c75cebe4af10663e3deb4201576c
Zisaac99 <zisaac2468@gmail.com>
2025-08-25 19:19:38 +0800

    Deadline.java: Update class for abstract & Storage

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -6,6 +6,17 @@ public class Deadline extends Task{
         this.dueBy = dueBy;
     }
 
+    public Deadline(String name, String dueBy, boolean isDone) {
+        this(name, dueBy);
+        setDone(isDone);
+    }
+
+    @Override
+    public String taskToString() {
+        return String.format("%s | %b | %s | %s", "D", getDone(), getName(), this.dueBy);
+    }
+
+
     @Override
     public String toString() {
         return String.format("[%s]%s (by: %s)", "D", super.toString(), this.dueBy);
