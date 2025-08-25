3a21fc29c57aca6c37fa2bb89bea5c71cfaaa1fa
Nguyen Quang Minh <minhmxc@gmail.com>
2025-08-26 03:49:51 +0800

    Add save feature

--- src/main/java/DeadlineTask.java
+++ src/main/java/DeadlineTask.java
@@ -1,26 +1,38 @@
+import java.util.ArrayList;
+
 /**
  * Encapsulate a deadline task.
  */
 class DeadlineTask extends Task {
-  /** Time that task need to be done before. */
-  private String deadline;
+    /** Time that task need to be done before. */
+    private String deadline;
 
-  /**
-   * Construct a basic task.
-   * 
-   * @param name     Name of task to be created.
-   * @param deadline Deadline of task to be created.
-   */
-  public DeadlineTask(String name, String deadline) {
-    super(name);
-    this.deadline = deadline;
-  }
+    /**
+     * Construct a basic task.
+     * 
+     * @param name Name of task to be created.
+     * @param deadline Deadline of task to be created.
+     */
+    public DeadlineTask(String name, String deadline) {
+        super(name);
+        this.deadline = deadline;
+    }
 
-  /**
-   * @return String representation of a deadline task.
-   */
-  @Override
-  public String toString() {
-    return String.format("[D]%s (by: %s)", super.toString(), this.deadline);
-  }
+    @Override
+    public ArrayList<String> toCommands() {
+        ArrayList<String> commands = new ArrayList<>();
+        commands.add(String.format("deadline %s /by %s", name, deadline));
+        if (status) {
+            commands.add(String.format("mark"));
+        }
+        return commands;
+    }
+
+    /**
+     * @return String representation of a deadline task.
+     */
+    @Override
+    public String toString() {
+        return String.format("[D]%s (by: %s)", super.toString(), this.deadline);
+    }
 }
--- src/main/java/TodoTask.java
+++ src/main/java/TodoTask.java
@@ -1,21 +1,33 @@
+import java.util.ArrayList;
+
 /**
  * Encapsulate a todo task.
  */
 class TodoTask extends Task {
-  /**
-   * Construct a basic task.
-   * 
-   * @param name Name of task to be created.
-   */
-  public TodoTask(String name) {
-    super(name);
-  }
+    /**
+     * Construct a basic task.
+     * 
+     * @param name Name of task to be created.
+     */
+    public TodoTask(String name) {
+        super(name);
+    }
 
-  /**
-   * @return String representation of a todo task.
-   */
-  @Override
-  public String toString() {
-    return String.format("[T]%s", super.toString());
-  }
+    @Override
+    public ArrayList<String> toCommands() {
+        ArrayList<String> commands = new ArrayList<>();
+        commands.add(String.format("todo %s", name));
+        if (status) {
+            commands.add(String.format("mark"));
+        }
+        return commands;
+    }
+
+    /**
+     * @return String representation of a todo task.
+     */
+    @Override
+    public String toString() {
+        return String.format("[T]%s", super.toString());
+    }
 }
