96fa81a8d0b5d84e668dee16990d93850fbccaee
Nguyen Quang Minh <minhmxc@gmail.com>
2025-08-26 19:21:31 +0800

    Refactor Storage

--- src/main/java/MinhGPT.java
+++ src/main/java/MinhGPT.java
@@ -1,66 +1,8 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MinhGPT {
-    /**
-     * Save the provided list of tasks 'tasks' into a file.
-     */
-    private static void saveTasks(ArrayList<Task> tasks) {
-        try {
-            FileWriter writer = new FileWriter("mem.txt");
-            String content = "";
-            for (int i = 0; i < tasks.size(); i++) {
-                ArrayList<String> commands = tasks.get(i).toCommands();
-                for (String cmd : commands) {
-                    content += content.equals("") ? cmd : "\n" + cmd;
-                }
-            }
-            writer.write(content);
-            writer.close();
-            Logger.info("Tasks are saved in mem.txt.");
-        } catch (IOException e) {
-            Logger.error(e.getMessage());
-            Logger.error("Tasks are not saved.");
-        }
-    }
-
-    /**
-     * Load list of tasks from disk if it exists.
-     */
-    private static ArrayList<Task> loadTasks() {
-        ArrayList<Task> tasks = new ArrayList<>();
-        try {
-            Scanner scanner = new Scanner(new File("mem.txt"));
-            while (scanner.hasNextLine()) {
-                String command = scanner.nextLine();
-                if (command.matches("^mark$")) {
-                    // Mark the last task as done
-                    if (tasks.size() > 0) {
-                        tasks.get(tasks.size() - 1).markAsDone();
-                    }
-                } else {
-                    try {
-                        tasks.add(Task.parseTask(command));
-                    } catch (ParseException e) {
-                        System.out.println("ERROR: memory file is corrupted.");
-                    }
-                }
-            }
-            scanner.close();
-            Logger.info(
-                    "Found mem.txt, loading tasks from previous sessions. To disable, run with --fresh flag.");
-        } catch (FileNotFoundException e) {
-            Logger.info("No memory file detected. Starting fresh.");
-        }
-
-        return tasks;
-    }
-
     /**
      * Main entry-point for the program.
      */
@@ -71,10 +13,12 @@ public class MinhGPT {
         // Initialisation
         Task.initialise();
         Ui ui = new Ui();
+        Storage storage = new Storage();
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> tasks = isFresh ? new ArrayList<>() : loadTasks();
+        ArrayList<Task> tasks = isFresh ? new ArrayList<>() : storage.loadTasks();
         ui.printWelcome();
 
+        // Main program loop
         while (true) {
             ui.printNext();
             String input = scanner.nextLine().trim();
@@ -83,7 +27,7 @@ public class MinhGPT {
             if (input.matches("^bye$")) {
                 // User want to exit
                 scanner.close();
-                saveTasks(tasks);
+                storage.saveTasks(tasks);
                 break;
             } else if (input.matches("^list$")) {
                 // List all tasks
@@ -126,6 +70,7 @@ public class MinhGPT {
             }
         }
 
+        // Cleaning up resources and exit
         scanner.close();
         ui.printExit();
     }
