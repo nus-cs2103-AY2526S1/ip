0b7d6414dad94acd6602f6874b091579b8a2829b
Nguyen Quang Minh <minhmxc@gmail.com>
2025-08-26 19:10:55 +0800

    Refactor Ui

--- src/main/java/MinhGPT.java
+++ src/main/java/MinhGPT.java
@@ -8,60 +8,6 @@ import java.util.Scanner;
 
 public class MinhGPT {
     /**
-     * Print the divider between each pair of input and response.
-     */
-    private static void printDivider() {
-        System.out.println("------------------------------------------");
-    }
-
-    /**
-     * Print empty lines.
-     * 
-     * @param size Number of empty lines to be printed.
-     */
-    private static void printPadding(int size) {
-        String str = "";
-        for (int i = 0; i < size; i++) {
-            str += "\n";
-        }
-        System.out.print(str);
-    }
-
-    /**
-     * Print the message when user first enter the program.
-     */
-    private static void printStartupMessage() {
-        String logo = """
-                  ███    ███ ██ ███    ██ ██   ██  ██████  ██████  ████████
-                  ████  ████ ██ ████   ██ ██   ██ ██       ██   ██    ██
-                  ██ ████ ██ ██ ██ ██  ██ ███████ ██   ███ ██████     ██
-                  ██  ██  ██ ██ ██  ██ ██ ██   ██ ██    ██ ██         ██
-                  ██      ██ ██ ██   ████ ██   ██  ██████  ██         ██
-                """;
-        printPadding(2);
-        System.out.print(logo);
-        printPadding(2);
-        System.out.print("Hello! I'm MinhGPT.\nWhat can I do for you?");
-        printPadding(2);
-    }
-
-    /**
-     * Print the message when user say 'bye'.
-     */
-    private static void printExitMessage() {
-        System.out.println("(╥﹏╥) Bye. Hope to see you again soon!");
-    }
-
-    /**
-     * Print the error message when the task with that index does not exist for mark / unmark and
-     * delete operation.
-     */
-    private static void printIndexError() {
-        System.out.println(
-                "<( ⸝⸝•̀ - •́⸝⸝)> There is no tasks with that index. You could have caused an IndexOutOfBoundsException.");
-    }
-
-    /**
      * Save the provided list of tasks 'tasks' into a file.
      */
     private static void saveTasks(ArrayList<Task> tasks) {
@@ -116,75 +62,6 @@ public class MinhGPT {
     }
 
     /**
-     * Keep polling inputs from users until they say 'bye'.
-     *
-     * @param initialTasks Initial list of tasks.
-     */
-    private static void program(ArrayList<Task> initialTasks) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Task> tasks = initialTasks;
-
-        while (true) {
-            printDivider();
-            System.out.print("Your input: ");
-            String input = scanner.nextLine().trim();
-            printPadding(1);
-
-            if (input.matches("^bye$")) {
-                // User want to exit
-                scanner.close();
-                saveTasks(tasks);
-                break;
-            } else if (input.matches("^list$")) {
-                // List all tasks
-                System.out.println(
-                        String.format("(˶˃ ᵕ ˂˶) Here are the list of tasks. You have %d in total.",
-                                tasks.size()));
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(String.format("%d.%s", i + 1, tasks.get(i)));
-                }
-            } else if (input.matches("^mark \\d+$")) {
-                // Mark a task as done
-                int index = Integer.parseInt(input.split("\\s+", 2)[1]) - 1;
-                if (index < 0 || index >= tasks.size()) {
-                    printIndexError();
-                    continue;
-                }
-                tasks.get(index).markAsDone();
-                System.out.println("(˵˃ ᗜ ˂˵) Congrats on finishing the task.");
-                System.out.println(tasks.get(index));
-            } else if (input.matches("^unmark \\d+$")) {
-                // Mark a task as undone
-                int index = Integer.parseInt(input.split("\\s+", 2)[1]) - 1;
-                if (index < 0 || index >= tasks.size()) {
-                    printIndexError();
-                    continue;
-                }
-                tasks.get(index).markAsUndone();
-                System.out.println("(¬`‸´¬) Huh? Why did you lie?");
-                System.out.println(tasks.get(index));
-            } else if (input.matches("^delete \\d+$")) {
-                // Delete a task
-                int index = Integer.parseInt(input.split("\\s+", 2)[1]) - 1;
-                if (index < 0 || index >= tasks.size()) {
-                    printIndexError();
-                }
-                System.out.println(String.format("(˶ᵔ ᵕ ᵔ˶) Removed: %s", tasks.get(index)));
-                tasks.remove(index);
-            } else {
-                // All other inputs are considered as trying to add a task
-                try {
-                    tasks.add(Task.parseTask(input));
-                    System.out.println(
-                            String.format("(˶ᵔ ᵕ ᵔ˶) Added: %s", tasks.get(tasks.size() - 1)));
-                } catch (ParseException e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }
-    }
-
-    /**
      * Main entry-point for the program.
      */
     public static void main(String[] args) {
@@ -193,12 +70,63 @@ public class MinhGPT {
 
         // Initialisation
         Task.initialise();
-        ArrayList<Task> initialTasks = isFresh ? new ArrayList<>() : loadTasks();
+        Ui ui = new Ui();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Task> tasks = isFresh ? new ArrayList<>() : loadTasks();
+        ui.printWelcome();
 
-        printStartupMessage();
+        while (true) {
+            ui.printNext();
+            String input = scanner.nextLine().trim();
+            ui.printSeperate();
 
-        program(initialTasks);
+            if (input.matches("^bye$")) {
+                // User want to exit
+                scanner.close();
+                saveTasks(tasks);
+                break;
+            } else if (input.matches("^list$")) {
+                // List all tasks
+                ui.printList(tasks);
+            } else if (input.matches("^mark \\d+$")) {
+                // Mark a task as done
+                int index = Integer.parseInt(input.split("\\s+", 2)[1]) - 1;
+                if (index < 0 || index >= tasks.size()) {
+                    ui.printIndexError();
+                } else {
+                    tasks.get(index).markAsDone();
+                    ui.printMark(tasks.get(index));
+                }
+            } else if (input.matches("^unmark \\d+$")) {
+                // Mark a task as undone
+                int index = Integer.parseInt(input.split("\\s+", 2)[1]) - 1;
+                if (index < 0 || index >= tasks.size()) {
+                    ui.printIndexError();
+                } else {
+                    tasks.get(index).markAsUndone();
+                    ui.printUnmark(tasks.get(index));
+                }
+            } else if (input.matches("^delete \\d+$")) {
+                // Delete a task
+                int index = Integer.parseInt(input.split("\\s+", 2)[1]) - 1;
+                if (index < 0 || index >= tasks.size()) {
+                    ui.printIndexError();
+                } else {
+                    ui.printDelete(tasks.get(index));
+                    tasks.remove(index);
+                }
+            } else {
+                // All other inputs are considered as trying to add a task
+                try {
+                    tasks.add(Task.parseTask(input));
+                    ui.printAdd(tasks.get(tasks.size() - 1));
+                } catch (ParseException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
 
-        printExitMessage();
+        scanner.close();
+        ui.printExit();
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,125 @@
+import java.util.ArrayList;
+
+public class Ui {
+    // NOTE: PRIVATE
+
+    /**
+     * Print empty lines.
+     *
+     * @param size Number of empty lines to be printed.
+     */
+    private void padding(int size) {
+        String str = "";
+        for (int i = 0; i < size; i++) {
+            str += "\n";
+        }
+        System.out.print(str);
+    }
+
+    /**
+     * Print the divider between each pair of input and response.
+     */
+    private void divider() {
+        System.out.println("------------------------------------------");
+    }
+
+    // NOTE: PUBLIC
+
+    /**
+     * Print the UI when user enter the program.
+     */
+    public void printWelcome() {
+        String logo = """
+                  ███    ███ ██ ███    ██ ██   ██  ██████  ██████  ████████
+                  ████  ████ ██ ████   ██ ██   ██ ██       ██   ██    ██
+                  ██ ████ ██ ██ ██ ██  ██ ███████ ██   ███ ██████     ██
+                  ██  ██  ██ ██ ██  ██ ██ ██   ██ ██    ██ ██         ██
+                  ██      ██ ██ ██   ████ ██   ██  ██████  ██         ██
+                """;
+        padding(2);
+        System.out.print(logo);
+        padding(2);
+        System.out.print("Hello! I'm MinhGPT.\nWhat can I do for you?");
+        padding(2);
+    }
+
+    /**
+     * Print the UI to prepare for next input from user.
+     */
+    public void printNext() {
+        divider();
+        System.out.print("Your input: ");
+    }
+
+    /**
+     * Print the seperation between user input and program response.
+     */
+    public void printSeperate() {
+        padding(1);
+    }
+
+    /**
+     * Print the error message when the task with that index does not exist for mark / unmark and
+     * delete operation.
+     */
+    public void printIndexError() {
+        System.out.println("<( ⸝⸝•̀ - •́⸝⸝)> There is no tasks with that index."
+                + " You could have caused an IndexOutOfBoundsException.");
+    }
+
+    /**
+     * Print the UI when user add a task.
+     *
+     * @param task Task that was just added by user.
+     */
+    public void printAdd(Task task) {
+        System.out.println(String.format("(˶ᵔ ᵕ ᵔ˶) Added: %s", task));
+    }
+
+    /**
+     * Print the UI when user mark a task.
+     *
+     * @param task Task that was just marked by user.
+     */
+    public void printMark(Task task) {
+        System.out.println("(˵˃ ᗜ ˂˵) Congrats on finishing the task.");
+        System.out.println(task);
+    }
+
+    /**
+     * Print the UI when user unmark a task.
+     *
+     * @param task Task that was just unmarked by user.
+     */
+    public void printUnmark(Task task) {
+        System.out.println("(¬`‸´¬) Huh? Why did you lie?");
+        System.out.println(task);
+    }
+
+    /**
+     * Print the UI when user delete a task.
+     *
+     * @param task Task that was just deleted by user.
+     */
+    public void printDelete(Task task) {
+        System.out.println(String.format("(˶ᵔ ᵕ ᵔ˶) Removed: %s", task));
+    }
+
+    /**
+     * Print the UI when user list all tasks.
+     */
+    public void printList(ArrayList<Task> tasks) {
+        System.out.println(String.format(
+                "(˶˃ ᵕ ˂˶) Here are the list of tasks. You have %d in total.", tasks.size()));
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.format("%d.%s", i + 1, tasks.get(i)));
+        }
+    }
+
+    /**
+     * Print the UI when user exit the program.
+     */
+    public void printExit() {
+        System.out.println("(╥﹏╥) Bye. Hope to see you again soon!");
+    }
+}
