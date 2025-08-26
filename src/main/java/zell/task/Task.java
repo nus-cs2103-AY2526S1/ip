80d3a17376df04b6fba0e62aa89113cf9e7259ae
Zisaac99 <zisaac2468@gmail.com>
2025-08-27 04:04:22 +0800

    Task.java: Add in error detection

--- src/main/java/zell/task/Task.java
+++ src/main/java/zell/task/Task.java
@@ -13,12 +13,29 @@ public abstract class Task {
 
     public static Task stringToTask(String stringTask) throws ZellException{
         String[] components = stringTask.split(" \\| ");
+
+        if (components.length < 1) {
+            throw new ZellException("Invalid task string provided! Certain parameters are missing.");
+        }
+
         switch (components[0]) {
         case "T":
+            if (components.length != 3) {
+                throw new ZellException("Invalid task string provided! Certain parameters are missing.");
+            }
+
             return new ToDo(components[2], Boolean.parseBoolean(components[1]));
         case "D":
+            if (components.length != 4) {
+                throw new ZellException("Invalid task string provided! Certain parameters are missing.");
+            }
+
             return new Deadline(components[2], components[3], Boolean.parseBoolean(components[1]));
         case "E":
+            if (components.length != 5) {
+                throw new ZellException("Invalid task string provided! Certain parameters are missing.");
+            }
+
             return new Event(components[2], components[3], components[4], Boolean.parseBoolean(components[1]));
         default:
             throw new ZellException("Unknown task type encountered when converting tasks");
