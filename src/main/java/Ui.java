dbb837ed2b0352c66462cef1a68251f8bba3e624
Zisaac99 <zisaac2468@gmail.com>
2025-08-27 00:00:41 +0800

    Ui.java: Shift UI logic here

---
+++ src/main/java/Ui.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ui {
+    private Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String readInput() {
+        return scanner.nextLine();
+    }
+
+    public void showMessage(String message) {
+        System.out.println(formatMessage(message));
+    }
+
+    public void showError(String message) {
+        System.out.println(formatMessage("Error: " + message));
+    }
+
+    private static String formatMessage(String message) {
+        return "________________________________________" +
+                "__________________________________________________\n" +
+                message +
+                "\n__________________________________" +
+                "________________________________________________________\n\n";
+    }
+}
