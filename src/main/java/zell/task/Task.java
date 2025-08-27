ce35f2e1a4edcef6b20d4e94afd12a7a27be71ec
Zisaac99 <zisaac2468@gmail.com>
2025-08-27 19:09:47 +0800

    Task.java: Add JavaDoc

--- src/main/java/zell/task/Task.java
+++ src/main/java/zell/task/Task.java
@@ -1,9 +1,15 @@
 package zell.task;
 
 import zell.exception.ZellException;
+import zell.storage.Storage;
 
+/**
+ * Represents a task for the Zell chatbot
+ */
 public abstract class Task {
+    /** The name of the task */
     private final String name;
+    /** Indicates whether a task is completed */
     private boolean isDone;
 
     public Task(String name) {
@@ -11,6 +17,17 @@ public abstract class Task {
         this.isDone = false;
     }
 
+    /**
+     * Converts a string stored in local storage to a task. Is a static method since it does not make sense to
+     * create an object does for doing this.
+     * <p>
+     * We split the string into different components by "|". The task is identified by the first component
+     * we should be either (T, D ,E). We also check based on the task is the number of components is correct.
+     * </p>
+     * @param stringTask The string stored in local storage to be converted back to a task.
+     * @return The correct Task based on the task string.
+     * @throws ZellException If an invalid task string is provided (wrong format).
+     */
     public static Task stringToTask(String stringTask) throws ZellException{
         String[] components = stringTask.split(" \\| ");
 
@@ -42,6 +59,12 @@ public abstract class Task {
         }
     }
 
+    /**
+     * Represents an abstract method to convert a task to a string to be stored for local storage. Will
+     * be implemented by subclasses that inherit task
+     *
+     * @return The string representation of the task to be stored.
+     */
     public abstract String taskToString();
 
 
@@ -57,6 +80,14 @@ public abstract class Task {
         return this.isDone;
     }
 
+    /**
+     * Overrides the default toString
+     * <p>
+     * Displays the task properly formatted with a checkbox which indicates if a task is completed. Used for
+     * display of the task to the user.
+     * </p>
+     * @return The toString format of the Task
+     */
     @Override
     public String toString() {
         String checked = this.isDone ? "X" : " ";
