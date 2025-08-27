d58b337a716ab3fc7934177f3e2ba97722d554b8
reineoeq <e1354688@u.nus.edu>
2025-08-28 00:05:23 +0800

    Add JavaDoc comments to Task.java and TaskList.java

--- src/main/java/tasks/Task.java
+++ src/main/java/tasks/Task.java
@@ -1,30 +1,60 @@
 package tasks;
 
+/**
+ * Abstract representation of a task.
+ */
 public abstract class Task {
     protected String description;
     protected boolean isDone;
     protected TaskType type;
 
+    /**
+     * Constructs a task with the given description and type.
+     *
+     * @param description the description of the task
+     * @param type the type of the task
+     */
     public Task(String description, TaskType type) {
         this.description = description;
         this.isDone = false;
         this.type = type;
     }
 
+    /**
+     * Returns the status icon for this task.
+     *
+     * @return "X" if done, otherwise a space
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Marks this task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Unmarks this task as not done.
+     */
     public void unmark() {
         this.isDone = false;
     }
 
+    /**
+     * Returns a string representation of this task suitable for saving to file.
+     *
+     * @return the file format string
+     */
     public abstract String toFileFormat();
 
+    /**
+     * Returns the string representation of the task for display.
+     *
+     * @return a formatted string representation of the task
+     */
     @Override
     public String toString() {
         return "[" + type.getSymbol() + "][" + getStatusIcon() + "] " + description;
