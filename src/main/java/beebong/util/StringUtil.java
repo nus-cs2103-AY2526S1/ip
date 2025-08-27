db7ee39ae76d9eec8d33061086ffa33e6a84f64c
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2025-08-27 19:30:26 +0800

    Add Tests for StringUtil

--- src/main/java/beebong/util/StringUtil.java
+++ src/main/java/beebong/util/StringUtil.java
@@ -2,13 +2,21 @@ package beebong.util;
 
 import java.util.Base64;
 
+import beebong.exception.BBongException;
+
 public class StringUtil {
     // Idea taken from chatGPT on how to safely design my serialization of strings
-    public static String encode(String str) {
+    public static String encode(String str) throws BBongException {
+        if (str == null) {
+            throw new BBongException("String to Encode is NULL!");
+        }
         return Base64.getEncoder().encodeToString(str.getBytes());
     }
 
-    public static String decode(String str) {
+    public static String decode(String str) throws BBongException {
+        if (str == null) {
+            throw new BBongException("String to Decode is NULL!");
+        }
         return new String(Base64.getDecoder().decode(str));
     }
 }
