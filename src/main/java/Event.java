ac064672258cb3ac0b742e1293a9f27a2b63f5b8
Chong Rui <chongrui2003@gmail.com>
2025-08-28 14:32:21 +0800

    Implement Level 7: persistent storage

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -6,6 +6,17 @@ public class Event extends Task {
         this.from = from;
         this.to = to;
     }
+    public Event(String description, boolean isDone, String fromTo) {
+        super(description, isDone);
+        String[] parts = fromTo.split(" ", 2);
+        if (parts.length == 2) {
+            this.from = parts[0];
+            this.to = parts[1];
+        } else {
+            this.from = "";
+            this.to = "";
+        }
+    }
 
     @Override 
     protected String kind() {
@@ -15,5 +26,9 @@ public class Event extends Task {
     @Override
     public String toString() {
         return kind() + status() + " " + super.description + " (from: " + from + " to: " + to + ")";
+    }
+    @Override
+    public String toSaveFormat() {
+        return String.format("E | %d | %s | %s", isDone() ? 1 : 0, description, from + " " + to);
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -6,6 +6,19 @@ public class Task {
         this.description = description;
         this.isDone = false; // default not done
     }
+    public Task(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+    
+    public boolean isDone() {
+        return isDone;
+    }
+    
+    public String toSaveFormat() {
+        // Default: T | 1 | description
+        return String.format("T | %d | %s", isDone ? 1 : 0, description);
+    }
 
     public void markDone() { 
         this.isDone = true; 
