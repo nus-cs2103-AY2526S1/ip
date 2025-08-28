4b1f5d8c0dcd9888123e5682c6a3ed1b2ad06c11
Haxatron <haxatron1@gmail.com>
2025-08-28 23:55:22 +0800

    Implement Level-1
    Rename class name to Jackbot.
    
    Implement echo feature.

--- src/main/java/Jackbot.java
+++ src/main/java/Jackbot.java
@@ -1,10 +1,31 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Jackbot {
     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        // Start session
         System.out.println("____________________________________________________________\n"
                          + " Hello! I'm Jackbot\n"
                          + " What can I do for you?\n"
-                         + "____________________________________________________________\n"
+                         + "____________________________________________________________\n");
+        // Echo back
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________\n" 
+                             + " " + input + "\n"
+                             + "____________________________________________________________\n");
+        }
+
+        // End session
+        System.out.println("____________________________________________________________\n" 
                          + " Bye. Hope to see you again soon!\n"
-                         + "____________________________________________________________");
+                         + "____________________________________________________________\n");
+        
+        sc.close();
     }
 }
