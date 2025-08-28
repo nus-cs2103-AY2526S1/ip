02ab655394c7644b2ac629c0688e86e317b920ee
Gokul <gokulfan123@gmail.com>
2025-08-28 22:28:56 +0800

    Add BadFileException

---
+++ src/main/java/BadFileException.java
@@ -0,0 +1,8 @@
+/// Bad file exception
+///
+/// @author Ravichandran Gokul
+public class BadFileException extends Exception {
+    public BadFileException(String message) {
+        super(message);
+    }
+}
