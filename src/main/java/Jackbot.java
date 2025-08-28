fefe8aee65a19a9d90c42974fabf797354e46399
Haxatron <haxatron1@gmail.com>
2025-08-29 00:03:07 +0800

    Refactor to use printFramed helper function
    Introduce new `printFramed` helper function to reduce code repetition.

--- src/main/java/Jackbot.java
+++ src/main/java/Jackbot.java
@@ -1,31 +1,34 @@
 import java.util.Scanner;
 
 public class Jackbot {
+
+    // Helper function to print framed messages
+    private static void printFramed(String msg) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" " + msg);
+        System.out.println("____________________________________________________________\n");
+    }
+    
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        
+
         // Start session
-        System.out.println("____________________________________________________________\n"
-                         + " Hello! I'm Jackbot\n"
-                         + " What can I do for you?\n"
-                         + "____________________________________________________________\n");
+        printFramed("Hello! I'm Jackbot\n"
+                  + "What can I do for you?\n");
+
+        
         // Echo back
         while (true) {
             String input = sc.nextLine();
             if (input.equalsIgnoreCase("bye")) {
                 break;
             }
-            System.out.println("____________________________________________________________\n" 
-                             + " " + input + "\n"
-                             + "____________________________________________________________\n");
+            printFramed(" " + input + "\n");
         }
 
         // End session
-        System.out.println("____________________________________________________________\n" 
-                         + " Bye. Hope to see you again soon!\n"
-                         + "____________________________________________________________\n");
-        
+        printFramed(" Bye. Hope to see you again soon!\n");
         sc.close();
     }
 }
