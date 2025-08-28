16f3f1328b02e18f80ce488697d49744da8411c9
evanjw2 <evanongjw@gmail.com>
2025-08-28 23:11:10 +0800

    Add JavaDoc comments for commands

--- src/main/java/evansbot/command/MarkCommand.java
+++ src/main/java/evansbot/command/MarkCommand.java
@@ -1,10 +1,15 @@
 package evansbot.command;
 
+import evansbot.Exceptions.EvansBotException;
 import evansbot.Exceptions.InvalidTaskIndexException;
 import evansbot.task.Storage;
 import evansbot.task.TaskList;
 import evansbot.ui.Ui;
 
+/**
+ * Represents a command to Mark a task in the TaskList.
+ * When executed, this command marks the Task in the TaskList depending on its Index.
+ */
 public class MarkCommand extends Command {
     private final int index;
 
@@ -12,6 +17,15 @@ public class MarkCommand extends Command {
         this.index = index;
     }
 
+    /**
+     * Executes the command to mark a Task in the TaskList by its Index.
+     * If either the index given is 0 or invalid, throws an InvalidTaskIndexException.
+     *
+     * @param tasks TaskList in which the new task will be added.
+     * @param ui User interface to interact with the user.
+     * @param storage Storage used to save the updated task list.
+     * @throws InvalidTaskIndexException If the index provided is invalid.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws InvalidTaskIndexException {
         try {
