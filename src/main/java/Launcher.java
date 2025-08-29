7430544851d80e381818f5e540cc55fdff94d1d2
vanphuc <e1375522@u.nus.edu>
2025-08-29 18:54:04 +0800

    Create file packages for all the file
    Organize the classes into suitable java packages and also import each class with the needed path

--- src/main/java/Launcher.java
+++
@@ -1,6 +0,0 @@
-public class Launcher {
-    public static void main(String[] args) throws PhucException {
-        Phuc phuc = new Phuc();
-        phuc.run();
-    }
-}
--- src/main/java/PhucException.java
+++
@@ -1,5 +0,0 @@
-public class PhucException extends Exception {
-    public PhucException(String errorMessage) {
-        super(errorMessage);
-    }
-}
