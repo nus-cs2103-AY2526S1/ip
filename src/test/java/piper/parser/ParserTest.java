f4618e188170f1cc0b31f2b10ebb4997c8f93f72
Ong Yi Xin <yixinong34@gmail.com>
2025-08-29 19:33:33 +0800

    Add Parser test

---
+++ src/test/java/piper/parser/ParserTest.java
@@ -0,0 +1,58 @@
+package piper.parser;
+
+import org.junit.jupiter.api.Test;
+import piper.PiperException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    @Test
+    void parse_splitsCommandAndArg() throws PiperException {
+        Parser.ParsedString ps = Parser.parse("deadline return book /by 2025-09-01");
+        assertEquals("deadline", ps.cmd);
+        assertEquals("return book /by 2025-09-01", ps.arg);
+    }
+
+    @Test
+    void parse_EmptyInput_exceptionThrown() {
+        PiperException e = assertThrows(PiperException.class, () -> Parser.parse(""));
+        assertEquals("CHIRP CHIRP! Don't think you said anything there. Try tweeting a command!", e.getMessage());
+    }
+
+    @Test
+    void parseIndex_acceptsNumeric() throws PiperException {
+        assertEquals(12, Parser.parseIndex("12"));
+    }
+
+    @Test
+    void parseIndex_trimsAndRejectsNonNumeric() {
+        PiperException e = assertThrows(PiperException.class, () -> Parser.parseIndex(" two "));
+        assertTrue(e.getMessage().toLowerCase().contains("numeric"));
+    }
+
+    @Test
+    void parseDeadlineArgs_success() throws PiperException {
+        Parser.DeadlineArgs da = Parser.parseDeadlineArgs("return book /by 2025-09-01");
+        assertEquals("return book", da.description);
+        assertEquals("2025-09-01", da.by);
+    }
+
+    @Test
+    void parseDeadlineArgs_missingByField_exceptionThrown() {
+        assertThrows(PiperException.class, () -> Parser.parseDeadlineArgs("return book /by "));
+    }
+
+    @Test
+    void parseEventArgs_success() throws PiperException {
+        Parser.EventArgs ea = Parser.parseEventArgs("trip /from 2025-09-05 /to 2025-09-10");
+        assertEquals("trip", ea.description);
+        assertEquals("2025-09-05", ea.from);
+        assertEquals("2025-09-10", ea.to);
+    }
+
+    @Test
+    void parseEventArgs_missingToField_exceptionThrown() {
+        assertThrows(PiperException.class, () -> Parser.parseEventArgs("trip /from 2025-09-05 /to "));
+    }
+}
