45c054a31734795a57f16d7ac3e4065f0bdae23c
Ong Yi Xin <yixinong34@gmail.com>
2025-08-29 17:33:06 +0800

    Create Parser

---
+++ src/main/java/piper/Parser.java
@@ -0,0 +1,102 @@
+package piper;
+
+public final class Parser {
+    private Parser() {}
+
+    public static final class ParsedString {
+        public final String cmd;
+        public final String arg;
+
+        public ParsedString(String cmd, String arg) {
+            this.cmd = cmd;
+            this.arg = arg;
+        }
+    }
+
+    public static final class DeadlineArgs {
+        public final String description;
+        public final String by;
+        public DeadlineArgs(String description, String by) {
+            this.description = description;
+            this.by = by;
+        }
+    }
+
+    public static final class EventArgs {
+        public final String description;
+        public final String from;
+        public final String to;
+        public EventArgs(String description, String from, String to) {
+            this.description = description;
+            this.from = from;
+            this.to = to;
+        }
+    }
+
+    public static ParsedString parse(String userInput) throws PiperException {
+        if (userInput == null || userInput.isEmpty()) {
+            throw new PiperException("CHIRP CHIRP! Don't think you said anything there. Try tweeting a command!");
+        }
+        String[] substrings = userInput.split("\\s", 2);
+        String cmd = substrings[0];
+
+        if (substrings.length < 2) {
+            if (cmd.equals("bye")) {
+                return new ParsedString(cmd, null);
+            } else if (cmd.equals("list")) {
+                return new ParsedString(cmd, null);
+            } else if (cmd.equals("todo") || cmd.equals("deadline") || cmd.equals("event")) {
+                // missing task description
+                throw new PiperException("TWEET TWEET! What are we supposed to do? Please specify the description!");
+            } else if (cmd.equals("mark") || cmd.equals("unmark") || cmd.equals("delete")) {
+                // missing task index
+                throw new PiperException("CHIRRUP! Which task should I peck at? Please give me the task index!");
+            } else {
+                // unrecognisable command
+                throw new PiperException("CHIRP CHIRP! I don't recognise the tune called '" + cmd + "'. Try another command?");
+            }
+        }
+
+        String arg = substrings[1];
+        return new ParsedString(cmd, arg);
+    }
+
+    public static DeadlineArgs parseDeadlineArgs(String arg) throws PiperException {
+        try {
+            String[] descriptionAndBy = arg.split("/by ", 2);
+            String description = descriptionAndBy[0].trim();
+            if (description.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            String by = descriptionAndBy[1].trim();
+            if (by.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            return new DeadlineArgs(description, by);
+        } catch (Exception e) {
+            throw new PiperException("OOPS! That deadline's off key. Please format the task as 'deadline (description) /by (deadline)'!");
+        }
+    }
+
+    public static EventArgs parseEventArgs(String arg) throws PiperException {
+        try {
+            String[] descriptionAndFrom = arg.split("/from ", 2);
+            String description = descriptionAndFrom[0].trim();
+            if (description.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            String[] fromAndTo = descriptionAndFrom[1].split("/to ", 2);
+            String from = fromAndTo[0].trim();
+            if (from.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            String to = fromAndTo[1].trim();
+            if (to.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+            return new EventArgs(description, from, to);
+        } catch (Exception e) {
+            throw new PiperException("EEP! Your event's missing a few notes. Please format the event as 'event (description) /from (start time) /to (end time)'!");
+        }
+    }
+}
--- src/main/java/piper/Piper.java
+++ src/main/java/piper/Piper.java
@@ -6,17 +6,12 @@ import piper.task.Task;
 import piper.task.Todo;
 import piper.task.Deadline;
 import piper.task.Event;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.List;
-import java.util.ArrayList;
 
 public class Piper { ;
     private static final String DATA_DIR = "data";
     private static final String DATA_FILE = "piper.txt";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws PiperException {
         final String CHATBOT_NAME = "Piper";
         Ui ui = new Ui(CHATBOT_NAME);
         TaskList tasks;
@@ -36,43 +31,27 @@ public class Piper { ;
 
         while (!exit) { // user is active
             String userInput = ui.read();
-            if (userInput == null) {
-                break;
-            }
             userInput = userInput.trim();
-            if (userInput.isEmpty()) {
-                ui.showError("CHIRP CHIRP! Don't think you said anything there. Try tweeting a command!");
-                continue;
-            }
-            try {
-                String[] substrings = userInput.split("\\s", 2);
-                String cmd = substrings[0];
 
-                if (substrings.length < 2) {
+            try {
+                Parser.ParsedString ps = Parser.parse(userInput);
+                String cmd = ps.cmd;
+                String arg = ps.arg;
+
+                if (arg == null) {
                     if (cmd.equals("bye")) { // user is inactive
                         ui.farewellUser();
                         exit = true;
                     } else if (cmd.equals("list")) {
                         // list all tasks
                         ui.displayTasks(tasks);
-                    } else if (cmd.equals("todo") || cmd.equals("deadline") || cmd.equals("event")) {
-                        // missing task description
-                        throw new PiperException("TWEET TWEET! What are we supposed to do? Please specify the description!");
-                    } else if (cmd.equals("mark") || cmd.equals("unmark") || cmd.equals("delete")) {
-                        // missing task index
-                        throw new PiperException("CHIRRUP! Which task should I peck at? Please give me the task index!");
-                    } else {
-                        // unrecognisable command
-                        throw new PiperException("CHIRP CHIRP! I don't recognise the tune called '" + cmd + "'. Try another command?");
                     }
                 } else {
-                    String remainingSubstring = substrings[1];
-
                     if (cmd.equals("mark") || cmd.equals("unmark")) {
                         // mark task as done or undone
 
                         try {
-                            int taskNumber = Integer.parseInt(remainingSubstring);
+                            int taskNumber = Integer.parseInt(arg);
                             int index = taskNumber - 1; // task list starts from index 1 but array list starts from index 0
                             Task task = tasks.getTask(index);
 
@@ -96,7 +75,7 @@ public class Piper { ;
                         // delete task
 
                         try {
-                            int taskNumber = Integer.parseInt(remainingSubstring);
+                            int taskNumber = Integer.parseInt(arg);
                             int index = taskNumber - 1;
                             Task task = tasks.getTask(index);
 
@@ -115,54 +94,21 @@ public class Piper { ;
                         Task task = null;
 
                         switch (cmd) {
-                            case "todo":
-                                task = new Todo(remainingSubstring);
-                                break;
-                            case "deadline": {
-                                try {
-                                    String[] descriptionAndBy = remainingSubstring.split("/by ", 2);
-                                    String description = descriptionAndBy[0].trim();
-                                    if (description.isEmpty()) {
-                                        throw new IllegalArgumentException();
-                                    }
-                                    String by = descriptionAndBy[1].trim();
-                                    if (by.isEmpty()) {
-                                        throw new IllegalArgumentException();
-                                    }
-                                    task = new Deadline(description, by);
-                                } catch (Exception e) {
-                                    throw new PiperException("OOPS! That deadline's off key. Please format the task as 'deadline (description) /by (deadline)'!");
-                                }
-                                break;
-                            }
-                            case "event": {
-                                try {
-                                    String[] descriptionAndFrom = remainingSubstring.split("/from ", 2);
-                                    String description = descriptionAndFrom[0].trim();
-                                    if (description.isEmpty()) {
-                                        throw new IllegalArgumentException();
-                                    }
-                                    String[] fromAndTo = descriptionAndFrom[1].split("/to ", 2);
-                                    String from = fromAndTo[0].trim();
-                                    if (from.isEmpty()) {
-                                        throw new IllegalArgumentException();
-                                    }
-                                    String to = fromAndTo[1].trim();
-                                    if (to.isEmpty()) {
-                                        throw new IllegalArgumentException();
-                                    }
-                                    task = new Event(description, from, to);
-                                } catch (Exception e) {
-                                    throw new PiperException("EEP! Your event's missing a few notes. Please format the event as 'event (description) /from (start time) /to (end time)'!");
-                                }
-                                break;
-                            }
+                        case "todo":
+                            task = new Todo(arg);
+                            break;
+                        case "deadline":
+                            Parser.DeadlineArgs da = Parser.parseDeadlineArgs(arg);
+                            task = new Deadline(da.description, da.by);
+                            break;
+                        case "event":
+                            Parser.EventArgs ea = Parser.parseEventArgs(arg);
+                            task = new Event(ea.description, ea.from, ea.to);
+                            break;
                         }
 
                         tasks.addTask(task);
-
                         storage.saveAll(tasks);
-
                         ui.showAddedTask(task);
                         ui.getTasksSize(tasks);
                     } else {
