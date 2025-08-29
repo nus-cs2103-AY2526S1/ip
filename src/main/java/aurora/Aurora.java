e14602434ebab6faac44dffb85aba59a5fcaf298
Dylan <152871121+bluemayo@users.noreply.github.com>
2025-08-29 16:08:10 +0800

    Add save method to write to file

--- src/main/java/aurora/Aurora.java
+++ src/main/java/aurora/Aurora.java
@@ -4,9 +4,13 @@ import aurora.command.Command;
 import aurora.command.CommandReader;
 import aurora.task.Task;
 
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+
+import java.io.File;
+import java.io.FileWriter;
 
 /**
  * Aurora is a chatbot that manages tasks.
@@ -36,7 +40,27 @@ public class Aurora {
         while (!input.equalsIgnoreCase("bye")) {
             Command command = CommandReader.read(input);
             speak(command.execute(list));
+            save(list);
             input = s.nextLine();
+        }
+    }
+
+    private static void save(List<Task> list) {
+        try {
+            File tasks = new File("./data/aurora.csv");
+            File parentDir = tasks.getParentFile();
+
+            if (!parentDir.exists() && !parentDir.mkdirs()) {
+                throw new IOException("Failed to create directory: " + parentDir);
+            }
+
+            try (FileWriter fw = new FileWriter(tasks)) {
+                for (Task task : list) {
+                    fw.write(task.toCSV());
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
