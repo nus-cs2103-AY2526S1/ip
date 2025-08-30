b920bec7ad63255a00f5be4cb59307df4d818078
Dylan <152871121+bluemayo@users.noreply.github.com>
2025-08-30 14:00:47 +0800

    Extract ui functions from main into Ui class

--- src/main/java/aurora/Aurora.java
+++ src/main/java/aurora/Aurora.java
@@ -5,49 +5,34 @@ import aurora.command.CommandReader;
 
 import aurora.storage.Storage;
 import aurora.task.Task;
-import aurora.task.TaskReader;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import aurora.ui.Ui;
 
 import java.util.Scanner;
 import java.util.List;
-import java.util.ArrayList;
 
 
 /**
  * Aurora is a chatbot that manages tasks.
  */
 public class Aurora {
-
-    private static final String INTRO = "Hello! I'm Aurora. What can I do for you?";
-    private static final String OUTRO = "Bye. Hope to see you again soon!";
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Ui ui = new Ui(new Scanner(System.in));
         Storage storage = new Storage("./data/aurora.txt");
         List<Task> list = storage.load();
 
-        speak(INTRO);
-        loop(scanner, list, storage);
-        speak(OUTRO);
-        scanner.close();
+        ui.speakIntro();
+        loop(ui, list, storage);
+        ui.speakOutro();
     }
 
-    private static void speak(String content) {
-        System.out.println("Aurora: " + content);
-    }
-
-    private static void loop(Scanner s, List<Task> list, Storage storage) {
-        String input = s.nextLine();
+    private static void loop(Ui ui, List<Task> list, Storage storage) {
+        String input = ui.readInput();
 
         while (!input.equalsIgnoreCase("bye")) {
             Command command = CommandReader.read(input);
-            speak(command.execute(list));
+            ui.speak(command.execute(list));
             storage.save(list);
-            input = s.nextLine();
+            input = ui.readInput();
         }
     }
 }
---
+++ src/main/java/aurora/ui/Ui.java
@@ -0,0 +1,27 @@
+package aurora.ui;
+
+import java.util.Scanner;
+
+public class Ui {
+    private final Scanner scanner;
+
+    public Ui(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void speak(String text) {
+        System.out.println("Aurora: " + text);
+    }
+
+    public void speakIntro() {
+        speak("Hello! I'm Aurora. What can I do for you?");
+    }
+
+    public void speakOutro() {
+        speak("Bye. Hope to see you again soon!");
+    }
+
+    public String readInput() {
+        return scanner.nextLine();
+    }
+}
