dddf9bf1ee38375ca581952627af65b1915dd3ab
Kineeeeee <e1375463@u.nus.edu>
2025-08-31 01:41:47 +0800

    Package as JAR file

--- src/test/java/sora/task/DeadlineTest.java
+++ src/test/java/sora/task/DeadlineTest.java
@@ -12,29 +12,29 @@ class DeadlineTest {
     public void toString_notDone_returnsCorrectFormat() {
         LocalDateTime by = LocalDateTime.of(2006, 2, 8, 23, 59);
         Deadline deadline = new Deadline("Do something", by);
-        assertEquals("[D][ ] Do something (by: Feb 08 2025 2359)", deadline.toString());
+        assertEquals("[D][ ] Do something (by: Feb 08 2006 2359)", deadline.toString());
     }
 
     @Test
     public void toString_markedDone_returnsCorrectFormat() {
         LocalDateTime by = LocalDateTime.of(2006, 2, 8, 23, 59);
-        Deadline deadline = new Deadline("Submit assignment", by);
+        Deadline deadline = new Deadline("Do something", by);
         deadline.markAsDone();
-        assertEquals("[D][X] Do something (by: Feb 08 2025 2359)", deadline.toString());
+        assertEquals("[D][X] Do something (by: Feb 08 2006 2359)", deadline.toString());
     }
 
     @Test
     public void toFormat_notDone_returnsCorrectFormat() {
         LocalDateTime by = LocalDateTime.of(2006, 2, 8, 23, 59);
-        Deadline deadline = new Deadline("Submit assignment", by);
-        assertEquals("D | 0 | Do something | Feb 08 2025 2359", deadline.toFormat());
+        Deadline deadline = new Deadline("Do something", by);
+        assertEquals("D | 0 | Do something | Feb 08 2006 2359", deadline.toFormat());
     }
 
     @Test
     public void toFormat_done_returnsCorrectFormat() {
         LocalDateTime by = LocalDateTime.of(2006, 2, 8, 23, 59);
-        Deadline deadline = new Deadline("Submit assignment", by);
+        Deadline deadline = new Deadline("Do something", by);
         deadline.markAsDone();
-        assertEquals("D | 1 | Do something | Feb 08 2025 2359", deadline.toFormat());
+        assertEquals("D | 1 | Do something | Feb 08 2006 2359", deadline.toFormat());
     }
 }
