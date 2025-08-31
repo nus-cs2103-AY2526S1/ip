977cd21b88ffe5011105d25f441bef0d39b9b0f8
Ong Yi Xin <yixinong34@gmail.com>
2025-08-31 20:49:16 +0800

    Add header comments for PiperException

--- src/main/java/piper/PiperException.java
+++ src/main/java/piper/PiperException.java
@@ -1,7 +1,16 @@
 package piper;
 
+/**
+ * Represents a recoverable application error.
+ * Used to signal invalid input or storage issues with user-friendly messages.
+ */
 public class PiperException extends Exception {
 
+    /**
+     * Creates a PiperException with the given message.
+     *
+     * @param message description of the error.
+     */
     public PiperException(String message) {
         super(message);
     }
