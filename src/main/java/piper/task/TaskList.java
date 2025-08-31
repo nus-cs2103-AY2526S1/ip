fbccc3e939ab5a58d6352875ae57b0cc40036822
Ong Yi Xin <yixinong34@gmail.com>
2025-08-31 20:48:28 +0800

    Add header comments for TaskList

--- src/main/java/piper/task/TaskList.java
+++ src/main/java/piper/task/TaskList.java
@@ -2,29 +2,53 @@ package piper.task;
 
 import java.util.ArrayList;
 
+/**
+ * A mutable list of Task objects with operations.
+ */
 public class TaskList {
     private final ArrayList<Task> tasks;
 
+    /**
+     * Creates an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Returns the number of tasks.
+     *
+     * @return list size.
+     */
     public int getSize() {
         return tasks.size();
     }
 
+    /**
+     * Returns the task at the given zero-based index.
+     *
+     * @param index position in the list.
+     * @return the task at index.
+     */
     public Task getTask(int index) {
         return tasks.get(index);
     }
 
-    public ArrayList<Task> getTasks() {
-        return tasks;
-    }
-
+    /**
+     * Adds a task to the end of the list.
+     *
+     * @param task task to add.
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Removes and returns the task at the given index.
+     *
+     * @param index position in the list.
+     * @return removed task.
+     */
     public Task deleteTask(int index) {
         return tasks.remove(index);
     }
