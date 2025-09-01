bd7f5c23d13222f86f3dcf49a60d67e78c9f8e68
Lunardrake <abc1a51h@gmail.com>
2025-09-01 21:09:34 +0800

    Add save file functionality and method headers

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -4,7 +4,19 @@ public class Todo extends Task {
         super(name, completed);
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public String print() {
         return "[T] [" + (this.completed ? "X" : " ") + "] " + this.name;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getAsCsv() {
+        return "T," + this.completed.toString() + "," + this.name;
     }
 }
