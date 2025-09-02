8d0e4a645ec6a38ee17b01a97e1ce95d48971fb1
Gareth Too <64114942+Gareth2YuSheng@users.noreply.github.com>
2025-09-02 23:15:39 +0800

    Add JavaDoc for Exception classes

--- src/main/java/beebong/exception/InvalidSerializedTaskDataException.java
+++ src/main/java/beebong/exception/InvalidSerializedTaskDataException.java
@@ -1,10 +1,21 @@
 package beebong.exception;
 
+/**
+ * Represents an Invalid Serialized Task Data exception used within the BeeBong application.
+ */
 public class InvalidSerializedTaskDataException extends BBongException {
+    /**
+     * Constructs a new {@code InvalidSerializedTaskDataException} with a pre-defined message.
+     */
     public InvalidSerializedTaskDataException() {
         super("Invalid Serialized Task Data!");
     }
 
+    /**
+     * Constructs a new {@code InvalidSerializedTaskDataException} with the specified detail message.
+     *
+     * @param message the detail message to describe the cause of the exception.
+     */
     public InvalidSerializedTaskDataException(String message) {
     super(message);
   }
