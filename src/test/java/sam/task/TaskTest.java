611c5936e37a20da59aba4e313b1594e7174c749
Chong Rui <chongrui2003@gmail.com>
2025-09-03 10:54:35 +0800

    Add JUnit tests for Task class

---
+++ src/test/java/sam/task/TaskTest.java
@@ -0,0 +1,25 @@
+package sam.task;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TaskTest {
+    
+    @Test
+    public void testTaskCreation() {
+        Task task = new Task("Test task");
+        String taskString = task.toString();
+        String description = taskString.substring(taskString.indexOf("]") + 2).trim();
+        assertEquals("Test task", description);
+        assertFalse(task.isDone());
+    }
+    
+    @Test
+    public void testMarkDone() {
+        Task task = new Task("Test task");
+        assertFalse(task.isDone());
+        
+        task.markDone();
+        assertTrue(task.isDone());
+    }
+}
