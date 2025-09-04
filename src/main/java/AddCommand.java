484e2c8334abddacdb1fc8e2688ad9694f1fefbd
x3zapper <142211386+x3zapper@users.noreply.github.com>
2025-09-05 02:59:16 +0800

    Add AddCommand

---
+++ src/main/java/AddCommand.java
@@ -0,0 +1,38 @@
+public class AddCommand extends Command {
+    private final CommandTypeEnum type;
+    private final String args;
+
+    public AddCommand(CommandTypeEnum type, String args) {
+        this.type = type;
+        this.args = args;
+    }
+
+    @Override
+    public void execute(TaskList tasks, ChashUi ui, ChashDb db) {
+        //Create task
+        Task task;
+        try {
+            task = TaskParser.fromChashString(this.type, this.args);
+        } catch (ChashException ex) {
+            ui.printErr(ex.getMessage());
+            return;
+        }
+
+        //Add task
+        tasks.add(task);
+
+        //Write db
+        try {
+            db.writeDb(tasks);
+        } catch (IOException ex) {
+            ui.printErr("Error accessing CHASHDB, all data in memory will be lost on exit");
+        }
+
+        //Print
+        ui.printMsg(String.format(
+            "Got it. I've added this task:\n  %s\nNow you have %d tasks in the list.",
+            task,
+            tasks.size()
+        ));
+    }
+}
