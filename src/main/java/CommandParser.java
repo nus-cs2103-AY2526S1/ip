7e171242afc090d47052814337898605165d6f58
x3zapper <142211386+x3zapper@users.noreply.github.com>
2025-09-05 03:26:41 +0800

    Add CommandParser

---
+++ src/main/java/CommandParser.java
@@ -0,0 +1,34 @@
+/** CHASH user data parser */
+public class CommandParser {
+    /**
+     * todo
+     * 
+     * @param fullCommand desc
+     * @return desc
+     * @throws ChashException desc
+     */
+    public static Command parse(String fullCommand) throws ChashException {
+        String[] tmp = fullCommand.split(" ", 2);
+
+        CommandTypeEnum cmd;
+        try {
+            cmd = CommandTypeEnum.valueOf(tmp[0].toUpperCase());
+        } catch (IllegalArgumentException ex) {
+            throw new ChashException("Invalid command: " + tmp[0]);
+        }
+        String args = (tmp.length > 1) ? tmp[1] : "";
+
+        //Switch expression supported from Java 14>
+        return switch (cmd) {
+        case CommandTypeEnum.BYE -> new ExitCommand();
+        case CommandTypeEnum.LIST -> new ListCommand();
+        case CommandTypeEnum.MARK -> new MarkCommand(args, true);
+        case CommandTypeEnum.UNMARK -> new MarkCommand(args, false);
+        case CommandTypeEnum.TODO -> new AddCommand(cmd, args);
+        case CommandTypeEnum.DEADLINE -> new AddCommand(cmd, args);
+        case CommandTypeEnum.EVENT -> new AddCommand(cmd, args);
+        case CommandTypeEnum.DELETE -> new DeleteCommand(args);
+        default -> throw new ChashException("Unsupported command");
+        }
+    }
+}
