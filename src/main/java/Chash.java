c7c3fdf9ce8cf2a31619d1d7bf255b39f78a96c3
x3zapper <142211386+x3zapper@users.noreply.github.com>
2025-09-05 04:31:48 +0800

    Update Chash to A-MoreOOP
    Integrated all extracted class functionalities back into Chash

--- src/main/java/Chash.java
+++ src/main/java/Chash.java
@@ -1,258 +1,46 @@
 //Imports
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
 /*
-Enum reference
-https://stackoverflow.com/a/3978690
-https://stackoverflow.com/a/604426
-https://stackoverflow.com/a/59608518
-https://stackoverflow.com/a/26118954
-https://stackoverflow.com/a/23128025
+Dependancy Priority:
+ChashException.java CommandTypeEnum.java Task.java TaskDateParser.java ChashUi.java
+Todo.java Deadline.java Event.java TaskList.java
+TaskParser.java 
+ChashDb.java 
+Command.java 
+ExitCommand.java ListCommand.java MarkCommand.java DeleteCommand.java AddCommand.java
+CommandParser.java
+Chash.java
 */
 
 public class Chash {
-    private static final String LINEINDENT = "    ";
-    private static final String LINESEP = "____________________________________________________________";
-    private enum ChatCommand {
-        BYE("bye"),
-        LIST("list"),
-        MARK("mark"),
-        UNMARK("unmark"),
-        TODO("todo"),
-        DEADLINE("deadline"),
-        EVENT("event"),
-        DELETE("delete"),
-        UNKNOWNCMD("unrecognized command")
-        ;
-
-        private final String text;
-
-        ChatCommand(String text) {
-            this.text = text;
-        }
-
-        @Override
-        public String toString() {
-            return this.text;
-        }
-    }
-
-    //Handles printing format
-    private static void chatPrint(String txt) {
-        //todo: does not check for empty txt string
-        Stream<String> lineStream = Stream.<String>of(txt.split("\n"));
-
-        System.out.println(Chash.LINEINDENT + Chash.LINESEP);
-        lineStream.forEach(line -> System.out.println(Chash.LINEINDENT + line));
-        System.out.println(Chash.LINEINDENT + Chash.LINESEP);
-    }
-
-    //LIST command
-    private static void printChatHist(List<Task> chatHist) {
-        //Can do some sanity check for whether there is any chat history items
-
-        String hist = "Here are the tasks in your list:\n";
-        int counter = 0;
-        for (Task item : chatHist) {
-            counter += 1;
-            hist += String.format(
-                "%d. %s\n",
-                counter,
-                item
-            );
-        }
-
-        //Note: for static internal methods, should this function call be preceeded by 
-        //the class name? e.g. Chash.chatPrint(...)
-        chatPrint(hist.stripTrailing());
-    }
-
-    private static Todo createTodo(String taskDetail) {
-        //Sanity check
-        if (taskDetail.isEmpty()) {
-            throw new ChashException("Todo no description");
-        }
-
-        return new Todo(taskDetail);
-    }
-
-    private static Deadline createDeadline(String taskDetail) {
-        String[] tmp = taskDetail.split(" /by ", 2);
-
-        //Sanity check
-        if (tmp.length != 2) {
-            throw new ChashException("Deadline keyword /by missing");
-        } else if (tmp[0].isEmpty()) {
-            throw new ChashException("Deadline no description");
-        } else if (tmp[1].isEmpty()) {
-            throw new ChashException("/by missing value");
-        }
-        
-        return new Deadline(tmp[0], tmp[1]);
-    }
-
-    private static Event createEvent(String taskDetail) {
-        //Note: User has to specify /from first, /to next, at the end of the line
-        //Consider using regex to perform advanced splits
-        String[] tmp = taskDetail.split(" /from ", 2);
-
-        //Sanity check
-        if (tmp.length != 2) {
-            throw new ChashException("Event keyword /from missing");
-        } else if (tmp[0].isEmpty()) {
-            throw new ChashException("Event no description");
-        }
-
-        String[] startEnd = tmp[1].split(" /to ", 2);
-
-        //Sanity check
-        if (startEnd.length != 2) {
-            throw new ChashException("Event keyword /to missing");
-        } else if (startEnd[0].isEmpty()) {
-            throw new ChashException("/from missing value");
-        } else if (startEnd[1].isEmpty()) {
-            throw new ChashException("/to missing value");
-        }
-        
-        return new Event(tmp[0], startEnd[0], startEnd[1]);
-    }
-
     //Entrypoint
     public static void main(String[] args) {
-        //Entry message
-        chatPrint("Hello! I'm Crysis Heir Activity Sentre Hepdesk (CHASH)." + 
-            "\n" + "What can I do for you?");
-
-        //Work loop vars
-        Scanner scanStdin = new Scanner(System.in);
-        boolean exitFlag = false;
-        ChashDb chashDb = new ChashDb();
-        ArrayList<Task> chatHist;
+        //Startup
+        ChashUi ui = new ChashUi();
+        ChashDb db = new ChashDb();
+        TaskList tasks;
         try {
-            chatHist = chashDb.loadDb();
+            tasks = new TaskList(db.loadDb(ui));
         } catch (IOException ex) {
-            System.err.println(
-                "Error accessing CHASHDB, all data in memory will be lost on exit"
-            );
-            chatHist = new ArrayList<Task>();
+            tasks = new TaskList();
+            ui.printErr(ex.getMessage());
         }
-        
+
+        //Start Crysis Heir Activity Sentre Hepdesk (CHASH)
+        ui.printWelcome();
+
         //Work loop
-        while (!exitFlag) {
-            String userInput = scanStdin.nextLine();
-            
-            //Look for any valid commands and set enum value
-            ChatCommand userCmd;
+        boolean isExit = false;
+        while (!isExit) {
+            String fullCommand = ui.readLine();
             try {
-                userCmd = ChatCommand.valueOf(
-                    userInput.split(" ", 2)[0].toUpperCase()
-                );
-            } catch (IllegalArgumentException ex) {
-                userCmd = ChatCommand.UNKNOWNCMD;
-            }
-
-            Task task;
-            switch (userCmd) {
-                case ChatCommand.BYE:
-                    exitFlag = true;
-                    chatPrint("Bye. Hope to see you again soon!");
-                    break;
-                case ChatCommand.LIST:
-                    printChatHist(chatHist);
-                    break;
-                
-                //Fall over cases
-                case ChatCommand.MARK:
-                case ChatCommand.UNMARK:
-                case ChatCommand.DELETE:
-                    /*
-                    Can fail in 3 ways
-                    1. user requested index is not integer
-                    2. index does not exist
-                    3. user did not provide an index
-                    */
-                    try {
-                        task = chatHist.get(
-                            Integer.valueOf(userInput.split(" ", 2)[1]) - 1
-                        );
-
-                        if (userCmd == ChatCommand.MARK) {
-                            chatPrint(String.format(
-                                "Nice! I've marked this task as done:\n  %s",
-                                task.setDone(true)
-                            ));
-                        } else if (userCmd == ChatCommand.UNMARK) {
-                            chatPrint(String.format(
-                                "OK, I've marked this task as not done yet:\n  %s",
-                                task.setDone(false)
-                            ));
-                        } else {
-                            chatHist.remove(task);
-                            chatPrint(String.format(
-                                "Noted. I've removed this task:\n  %s\nNow you have %d tasks in the list.",
-                                task,
-                                chatHist.size()
-                            ));
-                        }
-
-                        //Update ChashDb
-                        try {
-                            chashDb.writeDb(chatHist);
-                        } catch (IOException ex) {
-                            System.err.println(
-                                "Error accessing CHASHDB, all data in memory will be lost on exit"
-                            );
-                        }
-                    } catch (ArrayIndexOutOfBoundsException ex) {
-                        chatPrint("No item requested");
-                    } catch (NumberFormatException ex) {
-                        chatPrint("Please specify by item number");
-                    } catch (IndexOutOfBoundsException ex) {
-                        chatPrint("Item does not exist in the list");
-                    }
-                    break;
-                
-                case ChatCommand.TODO:
-                case ChatCommand.DEADLINE:
-                case ChatCommand.EVENT:
-                    try {
-                        String taskDetail = userInput.split(" ", 2)[1];
-                        if (userCmd == ChatCommand.TODO) {
-                            task = createTodo(taskDetail);
-                        } else if (userCmd == ChatCommand.DEADLINE) {
-                            task = createDeadline(taskDetail);
-                        } else {
-                            task = createEvent(taskDetail);
-                        }
-
-                        chatHist.add(task);
-                        chatPrint(String.format(
-                            "Got it. I've added this task:\n  %s\nNow you have %d tasks in the list.",
-                            task,
-                            chatHist.size()
-                        ));
-
-                        //Update ChashDb
-                        try {
-                            chashDb.writeDb(chatHist);
-                        } catch (IOException ex) {
-                            System.err.println(
-                                "Error accessing CHASHDB, all data in memory will be lost on exit"
-                            );
-                        }
-                    } catch (ArrayIndexOutOfBoundsException ex) {
-                        chatPrint("No details provided");
-                    } catch (ChashException ex) {
-                        chatPrint(ex.getMessage());
-                    }
-                    break;
-                
-                default:
-                    chatPrint("Unsupported Command: " + userInput);
+                Command cmd = CommandParser.parse(fullCommand);
+                cmd.execute(tasks, ui, db);
+                isExit = cmd.isExit();
+            } catch (ChashException ex) {
+                ui.printErr(ex.getMessage());
+                continue;
             }
         }
     }
