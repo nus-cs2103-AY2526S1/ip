e6f65b6c893c3ab6ec76dffb39949716d19c5a35
Ziming <166503118+Zmzzz3@users.noreply.github.com>
2025-09-05 23:09:20 +0800

    Reorganise LynxGeneral
    Change methods within LynxGeneral to fit the new structure. LynxGeneral
    remains an abstract class since its commands are considered more general
    and need not be tied to instances of other classes.

--- src/main/java/lynx/parser/LynxGeneral.java
+++ src/main/java/lynx/parser/LynxGeneral.java
@@ -1,144 +1,80 @@
 package lynx.parser;
 
-import java.time.LocalDateTime;
-
 import lynx.storage.LynxFileManager;
 import lynx.storage.LynxStorage;
 import lynx.storage.LynxTaskList;
 import objectclasses.exception.LynxException;
-import objectclasses.exception.MissingArgumentException;
-import objectclasses.formatter.LynxDateManager;
 import objectclasses.task.DeadlineTask;
 import objectclasses.task.EventTask;
 import objectclasses.task.TodoTask;
 
 /**
- * Contains methods to execute general commands.
+ * Contains methods for executing general commands.
  */
 public abstract class LynxGeneral {
 
     /**
      * Attempts to create the <code>log.txt</code> data file or load its contents if it exists.
      *
+     * @param fileManager <code>LynxFileManager</code> with the filepath to load from.
+     * @param taskList Task list to load to.
      * @return Message representing reload status.
      * @throws LynxException If reload is unsuccessful.
      */
-    public static String reload() throws LynxException {
-        LynxFileManager.createFile();
-        LynxStorage.loadTasks(LynxFileManager.readFromFile());
+    public static String reload(LynxFileManager fileManager, LynxTaskList taskList) throws LynxException {
+        fileManager.createFile();
+        LynxStorage.loadTasks(fileManager.readFromFile(), taskList);
         return "Load successful!";
     }
 
     /**
      * Attempts to save the task list to the <code>log.txt</code> data file. Creates the file if it does not exist.
      *
+     * @param fileManager <code>LynxFileManager</code> with the filepath to save to.
+     * @param taskList Task list to load from.
      * @return Message representing save status.
      * @throws LynxException If save is unsuccessful.
      */
-    public static String save() throws LynxException {
-        LynxFileManager.createFile();
-        LynxFileManager.writeToFile(LynxStorage.unloadTasks());
+    public static String save(LynxFileManager fileManager, LynxTaskList taskList) throws LynxException {
+        fileManager.createFile();
+        fileManager.writeToFile(LynxStorage.unloadTasks(taskList));
         return "Save successful!";
     }
 
     /**
-     * Creates a <code>TodoTask</code> and adds it to the task list.
+     * Creates a <code>TodoTask</code> and adds it to a task list.
      *
      * @param input User command in the form "todo [name]".
+     * @param taskList Task list to add task to.
      * @return String to indicate the task added.
      * @throws LynxException If command or name is invalid.
      */
-    public static String addTodo(String input) throws LynxException {
-        if (input.length() <= 4) {
-            throw new MissingArgumentException("todo");
-        }
-        String name = input.substring(4).trim();
-        if (name.isEmpty()) {
-            throw new LynxException("Please specify a task name.");
-        }
-        checkName(name);
-
-        TodoTask task = new TodoTask(name);
-        return LynxTaskList.addTask(task);
+    public static String addTodo(String input, LynxTaskList taskList) throws LynxException {
+        return taskList.addTask(TodoTask.of(input));
     }
 
     /**
-     * Creates a <code>DeadlineTask</code> and adds it to the task list.
+     * Creates a <code>DeadlineTask</code> and adds it to a task list.
      *
      * @param input User command in the form "deadline [name] /by [date]".
+     * @param taskList Task list to add task to.
      * @return String to indicate the task added.
      * @throws LynxException If command, name or date is invalid.
      */
-    public static String addDeadline(String input) throws LynxException {
-        if (input.length() <= 8) {
-            throw new MissingArgumentException("deadline");
-        }
-        String[] parts = input.substring(8).split(" /by ", 2);
-        if (parts.length < 2) {
-            throw new LynxException("Please specify a deadline using ' /by '.");
-        }
-        String name = parts[0].trim();
-        if (name.isEmpty()) {
-            throw new LynxException("Please specify a task name.");
-        }
-        checkName(name);
-
-        LocalDateTime by = LynxDateManager.parseDateTime(parts[1].trim());
-        DeadlineTask task = new DeadlineTask(name, by);
-        return LynxTaskList.addTask(task);
+    public static String addDeadline(String input, LynxTaskList taskList) throws LynxException {
+        return taskList.addTask(DeadlineTask.of(input));
     }
 
     /**
-     * Creates a <code>EventTask</code> and adds it to the task list.
+     * Creates a <code>EventTask</code> and adds it to a task list.
      *
      * @param input User command in the form "event [name] /from [start] /to [end]".
+     * @param taskList Task list to add task to.
      * @return String to indicate the task added.
      * @throws LynxException If command, name or date is invalid.
      */
-    public static String addEvent(String input) throws LynxException {
-        if (input.length() <= 5) {
-            throw new MissingArgumentException("event");
-        }
-        String[] parts = input.substring(5).split(" /from ", 2);
-        if (parts.length < 2) {
-            throw new LynxException("Please specify a start time using ' /from '.");
-        }
-        String[] timeSplit = parts[1].split(" /to ", 2);
-        if (timeSplit.length < 2) {
-            throw new LynxException("Please specify an end time using ' /to '.");
-        }
-        String name = parts[0].trim();
-        if (name.isEmpty()) {
-            throw new LynxException("Please specify a task name.");
-        }
-        checkName(name);
-
-
-        LocalDateTime from = LynxDateManager.parseDateTime(timeSplit[0].trim());
-        LocalDateTime to = LynxDateManager.parseDateTime(timeSplit[1].trim());
-        if (from.isAfter(to)) {
-            throw new LynxException("The start date/time cannot be after the end date/time.");
-        }
-        EventTask task = new EventTask(name, from, to);
-        return LynxTaskList.addTask(task);
-    }
-
-    /**
-     * Checks that a task name is not blank, does not contain the "/" character, and does not exceed 150 characters.
-     *
-     * @param name Name of the task to be checked.
-     * @throws LynxException If task name is invalid.
-     */
-    public static void checkName(String name) throws LynxException {
-        if (name.isBlank()) {
-            throw new LynxException("Task name cannot be blank.");
-        }
-        if (name.contains("/")) {
-            throw new LynxException("Task name cannot contain the \"/\" character.");
-        }
-        if (name.length() > 150) {
-            throw new LynxException("Task name cannot exceed 150 characters.");
-        }
+    public static String addEvent(String input, LynxTaskList taskList) throws LynxException {
+        return taskList.addTask(EventTask.of(input));
     }
 
 }
