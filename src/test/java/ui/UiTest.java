2d4459acf38e5cff659b822b4ca642b37d292020
reineoeq <e1354688@u.nus.edu>
2025-09-07 17:47:29 +0800

    Update UiTest.java

--- src/test/java/ui/UiTest.java
+++ src/test/java/ui/UiTest.java
@@ -2,9 +2,6 @@ package ui;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
 import org.junit.jupiter.api.Test;
 
 class UiTest {
@@ -12,24 +9,18 @@ class UiTest {
     @Test
     void testShowWelcomeMessage() {
         Ui ui = new Ui();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
 
-        ui.showWelcome();
+        String output = ui.showWelcome();
 
-        String output = outContent.toString();
         assertTrue(output.contains("hi! i'm rainy!"));
     }
 
     @Test
     void testShowByeMessage() {
         Ui ui = new Ui();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
 
-        ui.showBye();
+        String output = ui.showBye();
 
-        String output = outContent.toString();
         assertTrue(output.contains("bai bai"));
     }
 }
