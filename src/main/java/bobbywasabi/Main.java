0b3b005cb7a204b1e2b0ec6f423526d875b85234
MooMooYonders <181126455+MooMooYonders@users.noreply.github.com>
2025-09-14 01:52:22 +0800

    Add product name to window

--- src/main/java/bobbywasabi/Main.java
+++ src/main/java/bobbywasabi/Main.java
@@ -33,6 +33,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("BobbyWasabi");
             fxmlLoader.<MainWindow>getController().setBobbyWasabi(bobbywasabi);
             stage.show();
         } catch (IOException e) {
