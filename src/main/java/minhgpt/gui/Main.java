9769974fa59f3d6e82b9d29532e1be53ca8b66e6
Nguyen Quang Minh <minhmxc@gmail.com>
2025-09-16 19:32:25 +0800

    Add product screenshot

--- src/main/java/minhgpt/gui/Main.java
+++ src/main/java/minhgpt/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("MinhGPT");
             fxmlLoader.<MainWindow>getController().setMinhGpt(minhgpt);
             stage.show();
         } catch (IOException e) {
