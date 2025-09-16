f688fc67d26cb62383d1714b2873d08e57d5bcbf
evanjw2 <evanongjw@gmail.com>
2025-09-16 15:22:45 +0800

    Add betterGUI to the chatbot

--- src/main/java/evansbot/exceptions/InvalidTaskIndexException.java
+++ src/main/java/evansbot/exceptions/InvalidTaskIndexException.java
@@ -13,8 +13,8 @@ public class InvalidTaskIndexException extends EvansBotException {
      */
     public InvalidTaskIndexException(int max) {
         super(max == 0
-                ? "There are currently no tasks inside the task list. Please add a task first!"
-                : "There are only " + max + " tasks in the list! Please choose a number between 1 to " + max);
+                ? "Error! There are currently no tasks inside the task list. Please add a task first!"
+                : "Error! There are only " + max + " tasks in the list! Please choose a number between 1 to " + max);
         this.maxIndex = max;
     }
 
