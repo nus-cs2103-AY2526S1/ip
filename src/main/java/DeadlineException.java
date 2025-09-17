52bedceb6444a72cfab2ee26715bc8d4e6ea23b2
oysterboi <yanshengong@gmail.com>
2025-09-17 20:59:09 +0800

    Add error handling via Exceptions
    Error handling is currently hard-coded.
    
    Proper error handling will make the code more structured and easier to
    update when more features are added and more possible errors arise.
    
    Error handling lets us inform the user what went wrong with their
    inputs and helps them to figure out how to use the app properly.
    
    We can streamline error handling using Exceptions, which allows us to
    catch exceptions that occur at any point throughout the code.

---
+++ src/main/java/DeadlineException.java
@@ -0,0 +1,5 @@
+public class TaskException extends RuntimeException {
+    public TaskException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/TodoException.java
@@ -0,0 +1,5 @@
+public class InvalidDescriptionException extends RuntimeException {
+    public InvalidDescriptionException(String message) {
+        super(message);
+    }
+}
