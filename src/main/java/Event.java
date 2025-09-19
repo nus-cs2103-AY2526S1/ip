08638707737b91da56234ecc02cb340f5217aac7
oysterboi <yanshengong@gmail.com>
2025-09-19 19:25:53 +0800

    Add proper date/time format for data storage
    Duke currently accepts any string as the input for dates in Deadline
    and Event tasks, regardless of whether the string represents a date
    in any way.
    
    Change the type that we store it in from String to LocalDate, using
    the relevant parsing and printing methods to ensure standardised
    storage of info in the text file.
    
    This change of type allows us to make use of the date/time of tasks
    in meaningful ways in the future, for example, if we want to view the
    tasks in order of due soonest to latest.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,16 +1,20 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
 
-    protected String from;
-    protected String to;
+    protected LocalDate from;
+    protected LocalDate to;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDate.parse(from);
+        this.to = LocalDate.parse(to);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd MMM yyyy");
+        return "[E]" + super.toString() + " (from: " + from.format(dtf) + " to: " + to.format(dtf) + ")";
     }
 }
