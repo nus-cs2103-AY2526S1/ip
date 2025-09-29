99d8219954da7a1fc2ac4d6954c5ffcedd401f45
dylan <mixedberries92@gmail.com>
2025-09-30 01:20:49 +0800

    Update Deadline and Storage with date exception

--- src/main/java/david/task/Deadline.java
+++ src/main/java/david/task/Deadline.java
@@ -2,6 +2,9 @@ package david.task;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import david.exception.InvalidDateTimeException;
 
 /**
  * Represents a Task with a deadline.
@@ -15,12 +18,17 @@ public class Deadline extends Task {
      * @param text Task description.
      * @param by Task end DateTime.
      */
-    public Deadline(String text, String by) {
+    public Deadline(String text, String by) throws InvalidDateTimeException {
         super(text);
 
         assert by != null : "By should never be null";
-        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
-        this.by = LocalDateTime.parse(by, inputFormat);
+        try {
+            DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+            this.by = LocalDateTime.parse(by, inputFormat);
+        } catch (DateTimeParseException e) {
+            throw new InvalidDateTimeException("Eh your datetime format is wrong leh. Use this format lah: yyyy-MM-dd HHmm");
+        }
+
     }
 
     public LocalDateTime getBy() {
