9eb0764af99a80b0e070b632e5d5074f746794ba
XKMai <xkmai014@gmail.com>
2025-10-14 19:42:11 +0800

    Update Comments

--- src/main/java/iris/IrisException.java
+++ src/main/java/iris/IrisException.java
@@ -1,6 +1,6 @@
 package iris;
 
-// Custom exception class for Iris application
+/** Custom exception class for Iris application **/
 public class IrisException extends Exception {
     public IrisException(String message) {
         super(message);
